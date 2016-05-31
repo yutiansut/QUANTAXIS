3d7e5472b3afbfcbfd1e169364c7be96cf9dfdd5
yutian <yutiansut@qq.com>
2016-06-01 04:59:34 +0800

    ~

--- +FreeMarkets/+MultiDealer/DealerStrategy.m
+++ +FreeMarkets/+MultiDealer/DealerStrategy.m
@@ -3,12 +3,25 @@ classdef DealerStrategy < Handle
     % behavior of market dealers
     % 2016/6/1  
     properties
-    
+        
     end
     events
     
     end
     methods
+        function DS=DealerStrategy()
+            
+        end
+    end
     
+    methods(access='public')
+        function DS = RandomStrategy(DS,varargin)
+            DS.Price.Output=DS.Price.Input+(2*ceil(rand-0.5)-1)*0.5;
+            DS.Amount.Output=5000*(2*ceil(rand-0.5)-1);
+        end
+        function DS = RandomNStrategy(DS,varargin)
+            
+            
+        end
     end
 end
