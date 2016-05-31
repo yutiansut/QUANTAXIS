19a9626f9824cd6e6d6863b504984901f4ace2e7
yutian <yutiansut@qq.com>
2016-06-01 04:19:55 +0800

    ~

---
+++ +FreeMarkets/+MultiDealer/DealerStrategy.m
@@ -0,0 +1,12 @@
+classdef DealerStrategy < Handle
+    % by yutiansut
+    % behavior of market dealers
+    % 2016/6/1  
+    properties
+    
+    end
+    events
+    end
+    methods
+    end
+end
