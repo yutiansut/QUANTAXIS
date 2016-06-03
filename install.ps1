b5dba3677b4ec7588eba98f0e90a6a3c54c6b365
yutian <yutiansut@qq.com>
2016-06-03 19:23:49 +0800

    commit install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,1 @@
-cd D:
-mkdir quantaxis
-cd quantaxis
-git clone https://github.com/yutiansut/quantaxis
-
-cd quantaxis
 Copy-Item ('F:\QUANTAXIS\QUANTAXIS\Auxiliary\JDBC\mysql-connector-java-5.1.7-bin.jar')  ('C:\Program Files\MATLAB\R2016a\java\jar\toolbox')
