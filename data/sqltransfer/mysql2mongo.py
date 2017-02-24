5263c1712c835951a5fe34bf445464e4b0633d1b
yutiansut <yutiansut@qq.com>
2017-02-24 22:33:34 +0800

    新增时间函数

---
+++ data/sqltransfer/mysql2mongo.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+from WindPy import *
+import pymongo
+import pymysql
+import re
+
+client =pymongo.MongoClient('localhost', 27017)  
+db = client.stock
+coll = db.ts
+
+w.start()
+
+conn=pymysql.connect(host='127.0.0.1',port=3306,user='root',passwd='940809',db='quantaxis',charset='utf8')
+cursor=conn.cursor()
+cursor.execute("select * from stocklist")
+result = cursor.fetchall()
+days=w.tdays("2000-01-01", "2017-02-22", "")
+tradeday=days.Data
+for item in result:
+    code = item[0][0:6]
+    codename=item[1]
+    strs = code+'_ts'
+    print strs
+    cursorsx=conn.cursor()
+    sqlquery="select * from "+strs
+    print sqlquery
+    cursorsx.execute(sqlquery) 
+    resultsx = cursorsx.fetchall()
+    
+    for items in resultsx:
+        coll.insert({"code":code,"name":codename,"date":items[0],"pre_close":items[1],"open":items[2],"high":items[3],"low":items[4],"close":items[5],"volume":items[6],"amt":items[7],"chg":items[8],"pct_chg":items[9],"swing":items[10],"vwap":items[11],"turn":items[12],"rel_ipo_chg":items[13],"rel_ipo_pct_chg":items[14]})
+conn.commit()
+cursor.close()
+conn.close()
---
+++ data/wind/days.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+from WindPy import *
+
+from datetime import datetime
+import pymongo
+
+
+client =pymongo.MongoClient('localhost', 27017)  
+db = client.wind
+coll = db.days
+w.start()
+
+days=w.tdays("2000-01-01", "2017-02-22", "")
+tradeday=days.Data
+for item in tradeday:
+    coll.insert({"date":item})
