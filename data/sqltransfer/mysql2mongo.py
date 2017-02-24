581d60ce542f43422a1ba76678389e91a257352d
yutiansut <yutiansut@qq.com>
2017-02-24 22:39:32 +0800

    commit

--- data/sqltransfer/mysql2mongo.py
+++ data/sqltransfer/mysql2mongo.py
@@ -18,6 +18,7 @@ result = cursor.fetchall()
 days=w.tdays("2000-01-01", "2017-02-22", "")
 tradeday=days.Data
 for item in result:
+    i=0;
     code = item[0][0:6]
     codename=item[1]
     strs = code+'_ts'
@@ -29,7 +30,9 @@ for item in result:
     resultsx = cursorsx.fetchall()
     
     for items in resultsx:
-        coll.insert({"code":code,"name":codename,"date":items[0],"pre_close":items[1],"open":items[2],"high":items[3],"low":items[4],"close":items[5],"volume":items[6],"amt":items[7],"chg":items[8],"pct_chg":items[9],"swing":items[10],"vwap":items[11],"turn":items[12],"rel_ipo_chg":items[13],"rel_ipo_pct_chg":items[14]})
+        coll.insert({"code":code,"name":codename,"date":tradeday[i],"pre_close":items[1],"open":items[2],"high":items[3],"low":items[4],"close":items[5],"volume":items[6],"amt":items[7],"chg":items[8],"pct_chg":items[9],"swing":items[10],"vwap":items[11],"turn":items[12],"rel_ipo_chg":items[13],"rel_ipo_pct_chg":items[14]})
+        i=i+1
+        print i
 conn.commit()
 cursor.close()
 conn.close()
