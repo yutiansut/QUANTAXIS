4a1ec6949ffe8421cf2bbd34b7ca0fe4b1964bad
yutiansut <yutiansut@qq.com>
2017-03-12 11:31:32 +0800

    commit win

--- data/wind/getFutureData.py
+++ data/wind/getFutureData.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*
- import WindPy
- import pymongo
- import talib
+from WindPy import w
 
- 
+
+
+w.start()
+Data=w.wsd("RB1702.SHF", "open,high,low,close,volume", "2016-02-09", "2017-03-11", "TradingCalendar=SHFE;Fill=Previous")
+
+data=Data.Data;
+print data[0]
