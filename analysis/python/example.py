619499d6bacb220833b73731e550120af7bdee94
yutiansut <yutiansut@qq.com>
2017-03-23 22:10:47 +0800

    #320 Commit: example

--- analysis/python/example.py
+++ analysis/python/example.py
@@ -1,19 +1,26 @@
 import json
-import urllib
+import requests
 import sys
 import pymongo
 import talib
 from factor import *
 
 initFund=100000
+fund=[]
+# firstly, try something of bid price
+trade=requests.get("http://localhost:3000/backtest/ts?bidCode=000001&bidTime=2001-01-04&bidPrice=4.08")
+#firstLine = trade.readline() 
+assert trade.text == "success"
+
+class account():
+    def _init_(self):
+        fund[0]=initFund
+    def fund(self):
+        return
 
 
 
 
-trade=urllib.urlopen("http://localhost:3000/backtest/ts?bidCode=000001&bidTime=2001-01-04&bidPrice=4.08")
-firstLine = trade.readline() 
-print (firstLine)
 
 
-
-urllib.urlopen("http://localhost:3000/backtest/save?")
+#urllib.urlopen("http://localhost:3000/backtest/save?")
