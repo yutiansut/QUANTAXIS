9e66c27d2c52549d84af797d6839da4877238991
yutiansut <yutiansut@qq.com>
2017-04-03 02:43:22 +0800

    #380 Commit: 删除REST

--- REST/Data/datatask.py
+++
@@ -1,6 +0,0 @@
-# coding :utf-8
-
-import QAFetch
-
-def get_stock_day(name):
-    return QAFetch.get_stock_day(name,'2000-01-01','2017-04-02')
--- REST/Markets/futureMarkets.py
+++
@@ -1,3 +0,0 @@
-# coding:utf-8
-
-from QAMarket import deal
