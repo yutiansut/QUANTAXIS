0209e32ab4e1419f7743a1e1af03b1e633fbf65d
yutiansut <yutiansut@qq.com>
2017-04-03 03:19:42 +0800

    # 383 Commit: QAStrategy, QATask 修改

---
+++ QATasks/tasks.py
@@ -0,0 +1,14 @@
+from celery import Celery
+
+quantaxis = Celery('tasks', backend='amqp://guest@localhost//', broker='amqp://guest@localhost//')
+
+@quantaxis.task
+
+def save_data():
+  pass
+def update_data():
+  pass
+def update_spider(name):
+  pass
+def update_all_spider():
+  pass
--- tasks/config.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
-BROKER_URL = 'redis://127.0.0.1:6379/0'
