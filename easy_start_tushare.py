3dabdc3d23675034e7bf4e276615e680cd66b4bc
yutiansut <yutiansut@qq.com>
2017-04-22 21:21:43 +0800

    #558 Commit: 自动化测试

--- easy_start_tushare.py
+++ easy_start_tushare.py
@@ -7,3 +7,9 @@ QA.QA_save_stock_day_all()
 QA.QA_update_standard_sql()
 QA.QA_SU_save_stock_list('ts',pymongo.MongoClient())
 QA.QA_SU_save_trade_date_all()
+
+
+#仅仅是为了初始化才在这里插入用户,如果想要注册用户,要到webkit底下注册
+pymongo.MongoClient().quantaxis.user_list.insert({'username':'admin','password':'admin'})
+
+
--- test/test_strategy.py
+++ test/test_strategy.py
@@ -6,17 +6,19 @@ class backtest(QA.QA_Backtest):
     def init(self):
         self.account=QA.QA_Account()
         self.account.assets=10000
-        self.strategy_start_date='2010-01-10'
-        self.strategy_end_date='2015-01-28'
-        self.strategy_stock_list=['000001.SZ']
+        self.strategy_start_date='2015-01-10'
+        self.strategy_end_date='2017-1-28'
+        self.strategy_stock_list=['600592.SZ']
         self.strategy_gap=90
         self.account.account_cookie=str(random.random())
         self.setting.QA_util_sql_mongo_ip='127.0.0.1'
         self.setting.QA_setting_user_name='admin'
         self.setting.QA_setting_user_password='admin'
-        self.strategy_name='example'
+        self.strategy_name='Test-strategy'
         self.setting.QA_setting_init()
         self.account.init()
+        print(self.account.history_trade)
+        #input()
         QA.QA_util_log_info(self.setting.client)
         self.start_mes=QA.QA_util_realtime(self.strategy_start_date,self.setting.client)
         self.end_mes=QA.QA_util_realtime(self.strategy_end_date,self.setting.client)
@@ -49,9 +51,11 @@ class backtest(QA.QA_Backtest):
             #print(data['account']['body']['account']['cur_profit_present'])
             #print(data['market'][-1][6])
             
-            result=example_strategy(data['market'],data['account']['body']['account']['profit']*100,data['account']['body']['account']['hold'],data['account']['body']['account']['cur_profit_present']*100)
-            
-            if int(result)==1 and int(data['account']['body']['account']['hold'])==0:
+            result=predict(data['market'],data['account']['body']['account']['profit']*100,data['account']['body']['account']['hold'],data['account']['body']['account']['cur_profit_present']*100)
+            # print(result)
+
+            print(data['account']['body']['account']['hold'])
+            if result==1 and int(data['account']['body']['account']['hold'])==0:
                 #print(data['account']['body']['account']['assest_free'])
                 #print(data['market'][-1][4])
                 #self.bid.bid['amount']=int(float(data['account']['body']['account']['assest_free'])/float(data['market'][-1][4]))
@@ -66,9 +70,12 @@ class backtest(QA.QA_Backtest):
                 self.bid.bid['strategy']=self.strategy_name
                 message=self.market.market_make_deal(self.bid.bid,self.setting.client)
                 message=self.account.QA_account_receive_deal(message,self.setting.client)
+                self.backtest_message=message
                 QA.QA_SU_save_account_message(message,self.setting.client)
-                #print(message)
-            elif int(result)==1 and int(data['account']['body']['account']['hold'])==1:
+                #print('buy----------------------------------------------')
+                #QA.QA_util_log_info(message)
+                #input()
+            elif result==1 and int(data['account']['body']['account']['hold'])==1:
                 QA.QA_util_log_info('Hold and Watch!!!!!!!!!!!!')
                 ##
                 self.bid.bid['amount']=int(data['account']['body']['account']['portfolio']['amount'])
@@ -80,12 +87,11 @@ class backtest(QA.QA_Backtest):
                 self.bid.bid['strategy']=self.strategy_name
                 message=self.market.market_make_deal(self.bid.bid,self.setting.client)
                 message=self.account.QA_account_receive_deal(message,self.setting.client)
-                QA.QA_SU_save_account_message(message,self.setting.client)
-                #print(message)
-                 # todo  hold profit change
-            elif int(result)==0 and int(data['account']['body']['account']['hold'])==0:
-                 QA.QA_util_log_info('ZERO and Watch!!!!!!!!!!!!')
-            elif int(result)==0 and int(data['account']['body']['account']['hold'])==1:
+
+                # todo  hold profit change
+            elif result==0 and int(data['account']['body']['account']['hold'])==0:
+                QA.QA_util_log_info('ZERO and Watch!!!!!!!!!!!!')
+            elif result==0 and int(data['account']['body']['account']['hold'])==1:
                 self.bid.bid['amount']=int(data['account']['body']['account']['portfolio']['amount'])
                 self.bid.bid['price']=float(data['market'][-1][4])
                 self.bid.bid['code']=str(self.strategy_stock_list[0])[0:6]
@@ -95,13 +101,24 @@ class backtest(QA.QA_Backtest):
                 self.bid.bid['strategy']=self.strategy_name
 
                 message=self.market.market_make_deal(self.bid.bid,self.setting.client)
+                print('=================sell start')
+                print(message)
+                print('sell end==============')
                 message=self.account.QA_account_receive_deal(message,self.setting.client)
-                #print(message)
+                self.backtest_message=message
                 QA.QA_SU_save_account_message(message,self.setting.client)
-        self.backtest_message=message
+                #print('sell----------------------------------------------')
+                #QA.QA_util_log_info(message) 
+                #input()
+                #print(message)
+                #QA.QA_SU_save_account_message(message,self.setting.client)
+                #self.backtest_message=message
+               # input()
+            else:print('not enough data')
+        
         # 性能分析
         exist_time=int(self.end_mes['id'])-int(self.start_mes['id'])+1
-
+        print(self.backtest_message)
         performace=QA.QABacktest.QAAnalysis.QA_backtest_analysis_start(self.backtest_message,exist_time)
         backtest_mes={
             'user':self.setting.QA_setting_user_name,
@@ -119,24 +136,19 @@ class backtest(QA.QA_Backtest):
 
 
 
-
-
-
-def example_strategy(data,profit,hold,curprofit):
-    if hold==0:
+def predict(market,account_profit,if_hold,profit_per_trade):
+    #简单的策略 示例就是如果持有就买入,空仓就卖出
+    if if_hold==0:
         return 1
-    else: return 0
+    else:
+        return 0
 
 
-
-
-
-stock_list=pymongo.MongoClient().quantaxis.stock_list
-stoocks=stock_list.find_one()['stock']['code']
-for item in stoocks:
-    
+stock_list=['600592']
+# 可以是多个股票,也可以从数据库拿
+for item in stock_list:
+        
     BT=backtest()
     BT.init()
     BT.strategy_stock_list=[item]
     BT.handle_data()
-
