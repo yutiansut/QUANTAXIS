ae8d8fd5417ffb5385b686eebca981ea7d654325
yutiansut <yutiansut@qq.com>
2017-05-02 10:54:16 +0800

    #615 Commit: main.js

--- QUANTAXISWebkit/client/app/src/renderer/main.js
+++ QUANTAXISWebkit/client/app/src/renderer/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import VueResource from 'vue-resource'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 
 import App from './App.vue'
 import Todo from './components/todo.vue'
@@ -11,14 +11,14 @@ import Start from './components/Start.vue'
 
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
-import './assets/theme-carbon.css'
+import './theme-carbon.css'
 import echarts from 'echarts'
 
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 Vue.use(MuseUI)
-Vue.use(VueAwesomeSwiper)
+
 
 Vue.config.devtools = true;
 const routes = [ {
@@ -84,8 +84,7 @@ router.beforeEach((to, from, next) => {
 })
 
 
-new Vue({
-  el: '#app',  // el为实例提供挂载元素,值可以是css选择符,或者实际的html元素,或返回html元素的函数
+new Vue({ // el为实例提供挂载元素,值可以是css选择符,或者实际的html元素,或返回html元素的函数
   //如果实例在初始化的时候(比如现在)指定了这个选项,实例将立即进入编译过程,否则需要调用vm.$mount(),手动进行编译
   //注意元素只用作挂载点。如果提供了模板则元素被替换，除非 replace 为 false。元素可以用 vm.$el 访问
   //这里的el 绑定的是index.html下的div节点,id是app el :"#app"
@@ -97,4 +96,4 @@ new Vue({
   //render: function (createElement) {
   //return createElement(App)
   //}
-});
+}).$mount('#app')
