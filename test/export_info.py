c18aa608b96ee5ade2d46805c9dc7a8173515d76
yutiansut <yutiansut@qq.com>
2017-05-03 01:39:54 +0800

    #619 Commit:导出

--- test/export_info.py
+++ test/export_info.py
@@ -2,7 +2,8 @@
 import pymongo
 import csv
 coll=pymongo.MongoClient().quantaxis.backtest_info
-with open('info.csv','w') as f:
+with open('info.csv','w',newline='') as f:
     csvwriter=csv.writer(f)
+    csvwriter.writerow(['strategy','stock_list','start_time','end_time','account_cookie','total_returns','annualized_returns','benchmark_annualized_returns','win_rate','alpha','beta','sharpe','vol','benchmark_vol','max_drop','exist'])
     for item in  coll.find():
-        csvwriter.writerow([item['strategy'],str(item['stock_list'][0]),item['start_time'],item['end_time'],item['account_cookie'],item['total_returns'],item['annualized_returns'],item['benchmark_annualized_returns'],item['win_rate'],item['alpha'],item['beta'],item['sharpe'],item['vol'],item['benchmark_vol'],item['max_drop'],item['exist']])
+        csvwriter.writerow([item['strategy'],'c'+str(item['stock_list'][0]),item['start_time'],item['end_time'],item['account_cookie'],item['total_returns'],item['annualized_returns'],item['benchmark_annualized_returns'],item['win_rate'],item['alpha'],item['beta'],item['sharpe'],item['vol'],item['benchmark_vol'],item['max_drop'],item['exist']])
