711ab3a96c81a71617d5e49e1a00ef57ca80e8e6
yutiansut <yutiansut@qq.com>
2017-05-08 10:28:31 +0800

    优化数据库存储

--- QUANTAXIS/QASU/update_tushare.py
+++ QUANTAXIS/QASU/update_tushare.py
@@ -14,12 +14,14 @@ def QA_update_stock_day_all(code):
     pass
 
 def QA_update_standard_sql():
-    print('正在整理和更新数据,会有一段时间,请稍等.....')
+    print('正在整理和更新数据,请稍等.....')
     coll=pymongo.MongoClient().quantaxis.stock_day
     coll.ensure_index('code')
+    """
     for item in coll.find():
         date=item['date']
         date_stamp=QA_util_date_stamp(date)
         coll.update({"_id":item['_id']},{'$set':{'date_stamp':date_stamp}})
+    """
 
 
