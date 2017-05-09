a15175abaeb02d9f9c3c60c2a450b62706ebb27c
yutiansut <yutiansut@qq.com>
2017-05-09 15:53:11 +0800

    说明

--- QUANTAXIS/__init__.py
+++ QUANTAXIS/__init__.py
@@ -52,7 +52,7 @@ import QUANTAXIS.QACmd
 
 from QUANTAXIS.QACmd import QA_cmd
 import argparse
-QA_util_log_info('Welcome to QUANTAXIS, the Version is 0.3.9-dev-alpha')
+QA_util_log_info('Welcome to QUANTAXIS, the Version is 0.3.9-beta')
 
 
 def QA_help_fetch(self):
