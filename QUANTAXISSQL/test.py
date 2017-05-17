5d0070936396e034f42ff8361450409e4da22db0
yutiansut <yutiansut@qq.com>
2017-05-17 18:17:57 +0800

    @

--- QUANTAXISSQL/test.py
+++ QUANTAXISSQL/test.py
@@ -1,7 +1,12 @@
 from QASQL.qasql import Base
 test_db_name = 'test_database'
-def setUp(self):  # NOQA
-    self.first_record_id = 0
-    filter_db = Base(test_db_name, save_to_file=False)
-    filter_db.create('unique_id', 'name', "active", mode="override")
-    self.filter_db = filter_db
+
+first_record_id = 0
+filter_db = Base(test_db_name, save_to_file=False)
+filter_db.create('unique_id', 'name', "active", mode="override")
+filter_db = filter_db
+
+
+db = Base('dummy', save_to_file=False)
+db.create('name', 'age', 'size')
+db.insert(name='homer', age=23, size=1.84)
