efd3ef9beb76d0bd094127c7608ef72633edc301
yutiansut <yutiansut@qq.com>
2017-05-18 10:30:51 +0800

    #commit dict

---
+++ QUANTAXIS/QAMath/math_time_series.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+import pandas
+import numpy
+from .math_type import ensure_timeSerires
---
+++ QUANTAXIS/QAUtil/QAType.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+from QUANTAXIS.QAUtil.QADate import QA_util_date_valid
+def QA_util_ensure_timeSerires(data):
+    pass
+
+
+def QA_util_ensure_dict(data):
+    pass
+def QA_util_ensure_date(data):
+    
+    return QA_util_date_valid(data)
+def QA_util_ensure_ms(data):
+    pass
