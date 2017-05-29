37c1d13f26135f1a8293828d70da989c303a044c
yutiansut <yutiansut@qq.com>
2017-05-29 21:15:07 +0800

    #commit

--- QUANTAXISFuture/QUANTAXISTrade/QA_trade_stock/QA_trade_stock_api.py
+++ QUANTAXISFuture/QUANTAXISTrade/QA_trade_stock/QA_trade_stock_api.py
@@ -3,18 +3,27 @@
 import msvcrt
 import sys
 import TradeX
-
+import ConfigParser
 TradeX.OpenTdx()
+
+
+
 class QA_Trade_stock():
     def __init__(self):
-        self.sHost = ""
-        self.nPort = 7708
-        self.sVersion = ""
-        self.sBranchID =	1
-        self.sAccountNo = ""
-        self.sTradeAccountNo = ""
-        self.sPassword = ""
-        self.sTxPassword = ""
+        config=ConfigParser.ConfigParser()
+        try:
+            with open("setting.cfg","rw") as cfgfile: 
+                config.readfp(cfgfile)
+                self.sHost =config.get("trade","host")
+                self.nPort =config.get("trade","port")
+                self.sVersion =config.get("trade","version")
+                self.sBranchID =config.get("trade","branchID")
+                self.sAccountNo =config.get("trade","accountNo")
+                self.sTradeAccountNo =config.get("trade","tradeAccountNo")
+                self.sPassword=config.get("trade","password")
+                self.sTxPassword =config.get("trade","txPassword")
+        except:
+            print('error with read setting files')
     def QA_trade_stock_login(self):
         try:
             client = TradeX.Logon(self.sHost, self.nPort, self.sVersion, self.sBranchID, self.sAccountNo,self.sTradeAccountNo, self.sPassword, self.sTxPassword)
