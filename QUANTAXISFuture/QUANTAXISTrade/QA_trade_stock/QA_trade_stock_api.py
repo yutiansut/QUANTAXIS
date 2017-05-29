7657c4e910bc0d00512cf3de38cad1c4889d5322
yutiansut <yutiansut@qq.com>
2017-05-29 20:59:46 +0800

    #实盘修改

--- QUANTAXISFuture/QUANTAXISTrade/QA_trade_stock/QA_trade_stock_api.py
+++ QUANTAXISFuture/QUANTAXISTrade/QA_trade_stock/QA_trade_stock_api.py
@@ -7,18 +7,18 @@ import TradeX
 TradeX.OpenTdx()
 class QA_Trade_stock():
     def __init__(self):
-        sHost = ""
-        nPort = 7708
-        sVersion = ""
-        sBranchID =	1
-        sAccountNo = ""
-        sTradeAccountNo = ""
-        sPassword = ""
-        sTxPassword = ""
+        self.sHost = ""
+        self.nPort = 7708
+        self.sVersion = ""
+        self.sBranchID =	1
+        self.sAccountNo = ""
+        self.sTradeAccountNo = ""
+        self.sPassword = ""
+        self.sTxPassword = ""
     def QA_trade_stock_login(self):
         try:
-            self.client = TradeX.Logon(self.sHost, self.nPort, self.sVersion, self.sBranchID, self.sAccountNo,self.sTradeAccountNo, self.sPassword, self.sTxPassword)
-            return self.client
+            client = TradeX.Logon(self.sHost, self.nPort, self.sVersion, self.sBranchID, self.sAccountNo,self.sTradeAccountNo, self.sPassword, self.sTxPassword)
+            return client
         except TradeX.error as e:
             print ("error: " + e.message)
             sys.exit(-1)
