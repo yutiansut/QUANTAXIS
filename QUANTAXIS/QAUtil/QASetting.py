5b2880ff7e62e22942fe61e455e3f34d0c672979
yutiansut <yutiansut@qq.com>
2017-06-19 11:29:08 +0800

    #commit

--- QUANTAXIS/QAUtil/QASetting.py
+++ QUANTAXIS/QAUtil/QASetting.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 #
 # The MIT License (MIT)
 #
@@ -26,26 +26,32 @@ from QUANTAXIS.QAUtil import QA_util_log_info, QA_util_sql_mongo_setting
 
 
 class QA_Setting():
-    
-    QA_util_sql_mongo_ip='127.0.0.1'
-    QA_util_sql_mongo_port='27017'
-    client=QA_util_sql_mongo_setting(QA_util_sql_mongo_ip,QA_util_sql_mongo_port)
-    
-    QA_setting_user_name=''
-    QA_setting_user_password=''
-    user={'username':'','password':'','login':False}
+
+    def __init__(self):
+
+        self.QA_util_sql_mongo_ip = '127.0.0.1'
+        self.QA_util_sql_mongo_port = '27017'
+        self.client = QA_util_sql_mongo_setting(
+            self.QA_util_sql_mongo_ip, self.QA_util_sql_mongo_port)
+
+        self.QA_setting_user_name = ''
+        self.QA_setting_user_password = ''
+        self.user = {'username': '', 'password': '', 'login': False}
+
     def QA_setting_init(self):
-        self.client=QA_util_sql_mongo_setting(self.QA_util_sql_mongo_ip,self.QA_util_sql_mongo_port)
-        self.user=self.QA_setting_login()
+        self.client = QA_util_sql_mongo_setting(
+            self.QA_util_sql_mongo_ip, self.QA_util_sql_mongo_port)
+        self.user = self.QA_setting_login()
+
     def QA_setting_login(self):
-        self.username=self.QA_setting_user_name
-        self.password=self.QA_setting_user_password
-        QA_util_log_info('username:'+str(self.QA_setting_user_name))
-        result=QA_user_sign_in(self.username,self.password,self.client)
-        if result==True:
-            self.user['username']=self.username
-            self.user['password']=self.password
-            self.user['login']=True
+        self.username = self.QA_setting_user_name
+        self.password = self.QA_setting_user_password
+        QA_util_log_info('username:' + str(self.QA_setting_user_name))
+        result = QA_user_sign_in(self.username, self.password, self.client)
+        if result == True:
+            self.user['username'] = self.username
+            self.user['password'] = self.password
+            self.user['login'] = True
             return self.user
         else:
             QA_util_log_info('failed to login')
