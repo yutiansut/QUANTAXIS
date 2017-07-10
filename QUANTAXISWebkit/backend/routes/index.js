07f2ad62d99b03fa4f20a29e98aa06140c3b54a2
yutiansut <yutiansut@qq.com>
2017-07-10 23:08:31 +0800

    #一些web backend的修改

--- QUANTAXISWebkit/backend/routes/index.js
+++ QUANTAXISWebkit/backend/routes/index.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
-var axios = require('axios');
-var http = require('http');
-var events = require('events');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
