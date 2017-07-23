beaab994a140d8f8a74bbded01dfa4d133801d74
yutiansut <yutiansut@qq.com>
2017-07-23 21:15:40 +0800

    #test

--- test/test_different_bids.py
+++ test/test_different_bids.py
@@ -21,9 +21,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-import QUANTAXIS as QA
-
-
-collection=QA.QA_Setting.client.quantaxis.backtest_history
-collection.ensure_index('cookie')
