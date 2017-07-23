9824dec59b7a9dbe8153e5556306d7f54329090a
yutiansut <yutiansut@qq.com>
2017-07-23 21:11:13 +0800

    # 增加一个股票分钟回测的市场代码

--- QUANTAXIS/QAMarket/QAMarket_engine.py
+++ QUANTAXIS/QAMarket/QAMarket_engine.py
@@ -181,11 +181,145 @@ def market_stock_day_engine(__bid, fp=None):
     return __trading(__bid, __data)
 
 
-def market_stock_min_engine(__bid,fp=None):
+def market_stock_min_engine(__bid, fp=None):
     """
     time-delay stock trading engine
+
+
+    分钟线的成交和日线有几个不同:
+    分钟线不存在滑点的问题
+
+    如果当前报价>总成交量/4 按总成交量/4 计算
+
     """
-    pass
+    # data mod
+    # inside function
+    def __get_data(__bid):
+
+        __coll = QA_Setting.client.quantaxis.stock_min_5
+        __data = __coll.find_one(
+            {"code": str(__bid['code'])[0:6], "datetime": {'$gte': float(__bid['time'])}})
+        # 我们只需要找到这个时间点下一条数据就行
+        return __data
+    # trade mod
+
+    if fp == None:
+        __data = __get_data(__bid)
+    else:
+        __data = fp(__bid)
+
+    def __trading(__bid, __data):
+        """
+        trading system
+        """
+        try:
+            if __bid['price'] == 'market_price':
+                __bid_t = __bid
+                __bid_t['price'] = (float(__data["high"]) +
+                                    float(__data["low"])) * 0.5
+                return __trading(__bid_t, __data)
+
+            elif __bid['price'] == 'close_price':
+                __bid_t = __bid
+                __bid_t['price'] = float(__data["close"])
+                return __trading(__bid_t, __data)
+            else:
+                if __bid['amount_model'] == 'price':
+                    __bid_s = __bid
+                    __bid_s['amount'] = int(
+                        __bid['amount'] / (__bid['price'] * 100)) * 100
+                    __bid_s['amount_model'] = 'amount'
+                    return __trading(__bid_s, __data)
+                elif ((float(__bid['price']) < float(__data["high"]) and
+                       float(__bid['price']) > float(__data["low"])) or
+                      float(__bid['price']) == float(__data["low"]) or
+                      float(__bid['price']) == float(__data['high'])):
+                    if float(__bid['amount']) < float(__data['volume']) / 4:
+                        __deal_price = __bid['price']
+
+                    else:
+                        __bid['amount'] = float(__data['volume']) / 4
+                        if int(__bid['towards']) > 0:
+                            __deal_price = float(__data['high'])
+                        else:
+                            __deal_price = float(__data['low'])
+
+                    if int(__bid['towards']) > 0:
+                        __commission_fee = 0
+                    else:
+                        __commission_fee = 0.0005 * \
+                            float(__deal_price) * float(__bid['amount'])
+                        if __commission_fee < 5:
+                            __commission_fee = 5
+                    return {
+                        'header': {
+                            'source': 'market',
+                            'status': 200,
+                            'code': str(__bid['code']),
+                            'session': {
+                                'user': str(__bid['user']),
+                                'strategy': str(__bid['strategy'])
+                            },
+                            'order_id': str(__bid['order_id']),
+                            'trade_id': str(random.random())
+                        },
+                        'body': {
+                            'bid': {
+                                'price': str(__deal_price),
+                                'code': str(__bid['code']),
+                                'amount': int(__bid['amount']),
+                                'date': str(__bid['date']),
+                                'towards': int(__bid['towards'])
+                            },
+                            'market': {
+                                'open': __data['open'],
+                                'high': __data['high'],
+                                'low': __data['low'],
+                                'close': __data['close'],
+                                'volume': __data['volume'],
+                                'code': __data['code']
+                            },
+                            'fee': {
+                                'commission': float(__commission_fee)
+                            }
+                        }
+                    }
+
+        except:
+            return {
+                'header': {
+                    'source': 'market',
+                    'status': 500,
+                    'code': str(__bid['code']),
+                    'session': {
+                        'user': str(__bid['user']),
+                        'strategy': str(__bid['strategy'])
+                    },
+                    'order_id': str(__bid['order_id']),
+                    'trade_id': str(random.random())
+                },
+                'body': {
+                    'bid': {
+                        'price': 0,
+                        'code': str(__bid['code']),
+                        'amount': 0,
+                        'date': str(__bid['date']),
+                        'towards': __bid['towards']
+                    },
+                    'market': {
+                        'open': 0,
+                        'high': 0,
+                        'low': 0,
+                        'close': 0,
+                        'volume': 0,
+                        'code': 0
+                    },
+                    'fee': {
+                        'commission': 0
+                    }
+                }
+            }
+    return __trading(__bid, __data)
 
 
 def market_future_day_engine(__bid, fp=None):
@@ -199,5 +333,5 @@ def market_future_min_engine(__bid, fp=None):
     pass
 
 
-def market_future_tick_engine(__bid,fp=None):
+def market_future_tick_engine(__bid, fp=None):
     pass
