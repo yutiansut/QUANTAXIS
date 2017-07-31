98663d3723a610b86f795ad171680d1ddd3bd833
yutiansut <yutiansut@qq.com>
2017-07-31 21:45:06 +0800

    # version in the first

--- QUANTAXIS/__init__.py
+++ QUANTAXIS/__init__.py
@@ -31,8 +31,8 @@ by yutiansut
 
 2017/4/8
 """
-
-
+__version__ = '0.4.0-alpha-dev23'
+__author__ = 'yutiansut'
 
 # fetch methods
 
@@ -92,8 +92,8 @@ from QUANTAXIS.QACmd import QA_cmd
 import argparse
 
 
-__version__ = '0.4.0-alpha-dev23'
-__author__ = 'yutiansut'
+
+
 # 检查python版本:
 # 检查python版本
 import sys
