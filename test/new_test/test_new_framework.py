c61bb2edaa5efa889fda9977f0a221af607bb1f3
yutiansut <yutiansut@qq.com>
2017-08-03 16:50:19 +0800

    test modify

--- test/new_test/test_new_framework.py
+++ test/new_test/test_new_framework.py
@@ -1,12 +1,16 @@
 import QUANTAXIS as QA
-from QUANTAXIS import QA_Backtest_stock_day as QB
+from QUANTAXIS import QA_Backtest_stock_day
 
-
+QB=QA_Backtest_stock_day()
 @QB.backtest_init
 def init():
-        #
+    #
     QB.setting.QA_util_sql_mongo_ip='192.168.4.189'
-    QB.account.init_assest=250000
+
+    QB.account.init_assest=2500000
+    QB.benchmark_code='hs300'
+
+    QB.strategy_stock_list=['000001','000002','600010','601801','603111']
     QB.strategy_start_date='2017-03-01'
     QB.strategy_end_date='2017-07-01'
     QB.backtest_bid_model='market_price'
