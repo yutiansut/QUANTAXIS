85a72752d401281be58a8cc1c523fe6ba0f7b3e8
yutiansut <yutiansut@qq.com>
2017-08-06 00:23:39 +0800

    #commit qarisk

--- QUANTAXIS/__init__.py
+++ QUANTAXIS/__init__.py
@@ -31,7 +31,7 @@ by yutiansut
 
 2017/4/8
 """
-__version__ = '0.4.0-b-dev11'
+__version__ = '0.4.0-b-dev12'
 __author__ = 'yutiansut'
 
 # fetch methods
@@ -64,6 +64,9 @@ from QUANTAXIS.QAMarket import (QA_QAMarket_bid, QA_Market)
 # Account,Risk,Portfolio
 
 from QUANTAXIS.QAARP import QA_Account, QA_Portfolio, QA_Risk
+from QUANTAXIS.QAARP.QARisk import (QA_risk_account_freeCash_currentAssest,
+                                    QA_risk_account_freeCash_frozenAssest,
+                                    QA_risk_account_freeCash_initAssest, QA_risk_eva_account)
 # Backtest
 from QUANTAXIS.QABacktest.QABacktest import QA_Backtest, QA_Backtest_min
 from QUANTAXIS.QABacktest.QABacktest_stock_day import QA_Backtest_stock_day
@@ -77,7 +80,7 @@ from QUANTAXIS.QAUtil.QAType import (
     QA_util_ensure_date, QA_util_ensure_dict, QA_util_ensure_ms, QA_util_ensure_timeSerires)
 from QUANTAXIS.QAUtil import (QA_util_sql_mongo_setting, QA_util_cfg_initial, QA_util_realtime,
                               QA_util_id2date, QA_util_is_trade, trade_date_sse,
-                              QA_util_date_int2str,QA_util_date_str2int,
+                              QA_util_date_int2str, QA_util_date_str2int,
                               QA_util_date_stamp, QA_util_time_stamp, QA_util_ms_stamp,
                               QA_util_log_debug, QA_util_log_expection, QA_util_log_info,
                               QA_start_initial, QA_Setting, QA_util_get_date_index,
@@ -93,8 +96,6 @@ import QUANTAXIS.QACmd
 
 from QUANTAXIS.QACmd import QA_cmd
 import argparse
-
-
 
 
 # 检查python版本:
