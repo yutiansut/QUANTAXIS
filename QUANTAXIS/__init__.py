750e6772b4cac90047395ef6e1c8c336caba04a3
yutiansut <yutiansut@qq.com>
2017-08-06 00:17:02 +0800

    #version

--- QUANTAXIS/__init__.py
+++ QUANTAXIS/__init__.py
@@ -31,7 +31,7 @@ by yutiansut
 
 2017/4/8
 """
-__version__ = '0.4.0-beta-dev9'
+__version__ = '0.4.0-b-dev11'
 __author__ = 'yutiansut'
 
 # fetch methods
--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     install_requires=['pandas>=0.20', 'numpy>=1.12.0', 'tushare>=0.7.4', 'flask_socketio>=2.9.0 ',
-                      'lxml', ' beautifulsoup4',
+                      'lxml', ' beautifulsoup4','flask-socketio','flask',
                       'pymongo>=3.4', 'celery>=4.0.0', 'six>=1.10.0', 'tabulate>=0.7.7', 'pytdx>=1.10'
                       'docopt>=0.6.2', 'zenlog>=1.1', 'delegator.py>=0.0.12', 'flask>=0.12.2'],
     entry_points={
