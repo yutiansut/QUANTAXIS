903d7a1b41a0bb3563186a0d584bbb8c520c1099
yutiansut <yutiansut@qq.com>
2017-08-05 22:56:38 +0800

    #some check

--- QUANTAXIS_DataTools/QA_Data_Commpare/ths_spider.py
+++ QUANTAXIS_DataTools/QA_Data_Commpare/ths_spider.py
@@ -8,7 +8,9 @@ import numpy as np
 
 def get_k_data_year(code,year,if_fq):
     data_=[]
-    url='http://d.10jqka.com.cn/v2/line/hs_%s/%s/%s.js'%(code,if_fq,year)
+
+
+    url='http://d.10jqka.com.cn/v2/line/hs_%s/%s/%s.js'%(str(code),str(if_fq),str(year))
     for item in requests.get(url).text.split('\"')[3].split(';'):
         data_.append(item.split(','))
     return pd.DataFrame(data_,index=list(np.asarray(data_).T[0]),columns=['date','open','high','low','close','volume','amount','factor'])
