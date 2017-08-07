e6ce59ff1ff65cddcbe4cf56cf3fe4be8cf6ba75
yutiansut <yutiansut@qq.com>
2017-08-07 18:06:25 +0800

    # 更新一个  数据更新的时候的bug

--- QUANTAXIS/QASU/update_tushare.py
+++ QUANTAXIS/QASU/update_tushare.py
@@ -43,16 +43,22 @@ def QA_update_stock_day_all(client):
         QA_util_log_info('updating stock data -- %s'%item)
         #coll.find({'code':str(item)[0:6]}).count()
         #先拿到最后一个记录的交易日期
-        start_date=str(coll_stock_day.find({'code':str(item)[0:6]})[coll_stock_day.find({'code':str(item)[0:6]}).count()-1]['date'])
-        end_date=str(datetime.date.today())
 
-        QA_util_log_info('trying updating from %s to %s' %(start_date,end_date))
-        data=QATushare.QA_fetch_get_stock_day(str(item)[0:6],start_date,end_date)[1::]
+        if coll_stock_day.find({'code':str(item)[0:6]}).count()>0:
+            # 加入这个判断的原因是因为如果股票是刚上市的 数据库会没有数据 所以会有负索引问题出现
+            start_date=str(coll_stock_day.find({'code':str(item)[0:6]})[coll_stock_day.find({'code':str(item)[0:6]}).count()-1]['date'])
+            end_date=str(datetime.date.today())
+
+            QA_util_log_info('trying updating from %s to %s' %(start_date,end_date))
+            data=QATushare.QA_fetch_get_stock_day(str(item)[0:6],start_date,end_date)[1::]
+        else:
+            # 这时候直接更新拿到所有的数据就好了
+            data=QATushare.QA_fetch_get_stock_day(str(item)[0:6])
         try:
             coll_stock_day.insert_many(data)
         except:
             QA_util_log_info('error in updating--- %s'%item)
-            
+                
 
 
 
