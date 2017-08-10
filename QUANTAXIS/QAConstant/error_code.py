b07e1a9feb6992376b1803b5d4e0f4f7f69cacda
yutiansut <yutiansut@qq.com>
2017-08-10 18:35:07 +0800

    #cfg

--- QUANTAXIS/QAConstant/error_code.py
+++ QUANTAXIS/QAConstant/error_code.py
@@ -22,39 +22,36 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 class QUANTAXIS_Error():
-    QUANTAXIS_Error_framework_error          = 0100
-
-
+    QUANTAXIS_Error_framework_error = 0100
 
 # QUANTAXIS 框架错误代码 0000-0999
-QUANTAXIS_error={}
+QUANTAXIS_error = {}
 # 数据获取/数据库读取错误代码 1000-1999
-QA_fetch_error={}
+QA_fetch_error = {}
 # 数据错误代码 保存部分 2000-2499
-QA_save_error={}
+QA_save_error = {}
 
 # 数据错误代码  更新部分 2500-2599
-QA_update_error={}
+QA_update_error = {}
 # 交易错误代码(模拟market返回) 3000-3999
-QA_market_error={}
+QA_market_error = {}
 
 # 回测错误代码 4000-4999
-QA_backtest_error={
-    # 通用错误代码 
-    'total':{
+QA_backtest_error = {
+    # 通用错误代码
+    'total': {
 
     },
     # 特定错误代码
-    'QA_backtest_stock_day':{
+    'QA_backtest_stock_day': {
 
     },
-    'QA_backtest_stock_min':{},
-    'QA_backtest_stock_tick':{},
-    'QA_backtest_future_day':{},
-    'QA_backtest_future_min':{},
-    'QA_backtest_future_tick':{}
+    'QA_backtest_stock_min': {},
+    'QA_backtest_stock_tick': {},
+    'QA_backtest_future_day': {},
+    'QA_backtest_future_min': {},
+    'QA_backtest_future_tick': {}
 
 }
-
-
