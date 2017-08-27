0f58f78733b1e5b1fef75746080490b00b407b74
yutiansut <yutiansut@qq.com>
2017-08-28 00:44:39 +0800

    # commit 存储方式修改

--- QUANTAXIS/QASU/save_tdx.py
+++ QUANTAXIS/QASU/save_tdx.py
@@ -22,13 +22,38 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from QUANTAXIS.QAFetch.QATdx import QA_fetch_get_stock_day,QA_fetch_get_stock_min,QA_fetch_get_stock_xdxr,QA_fetch_get_stock_transaction
+from QUANTAXIS.QAFetch.QATdx import QA_fetch_get_stock_day, QA_fetch_get_stock_min, QA_fetch_get_stock_xdxr, QA_fetch_get_stock_transaction
+from QUANTAXIS.QAUtil import QA_util_to_json_from_pandas, QA_Setting,QA_util_log_info
+from QUANTAXIS.QAFetch.QATushare import QA_fetch_get_stock_list
+import datetime
 
 
-def save_stock_day():
-    #QA_fetch_get_stock_day(code)
 
-    pass
+def QA_SU_save_stock_all(client=QA_Setting.client):
+    __stock_list=QA_fetch_get_stock_list()
+    __coll = client.quantaxis.stock_day
+    __coll.ensure_index('code')
+    __err=[]
+    def __saving_work(code):
+        QA_util_log_info('Now Saving ==== %s' % (code))
+        try:
+            __coll.insert_many(
+                QA_util_to_json_from_pandas(
+                    QA_fetch_get_stock_day(
+                        code, '1990-01-01', str(datetime.date.today()), '00')))
+        except:
+            __err.append(code)
+    for i_ in range(len(__stock_list)):
+        QA_util_log_info('The %s of Total %s' % (i_, len(__stock_list)))
+        QA_util_log_info('DOWNLOAD PROGRESS %s ' % str(
+            float(i_ / len(__stock_list) * 100))[0:4] + '%')
+        __saving_work(__stock_list[i_])
+
+    if len(__err)>0:
+        QA_util_log_info('ERROR! Try Again with \n')
+        QA_util_log_info(__err)
+        for i__ in __err:
+            __saving_work(i__)
 
 if __name__ == '__main__':
-    pass
+    QA_SU_save_stock_all()
