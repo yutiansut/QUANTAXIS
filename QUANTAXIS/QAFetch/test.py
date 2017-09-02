fd09f29d2b1a3c7c4625311ba552511421b0a4f2
yutiansut <yutiansut@qq.com>
2017-09-02 18:22:42 +0800

    test_tdx.py

--- QUANTAXIS/QAFetch/test.py
+++ QUANTAXIS/QAFetch/test.py
@@ -1,36 +1,54 @@
 import QUANTAXIS as QA
-
-# stock_day
-# 不复权  
-# data=QA.QAFetch.QATdx.QA_fetch_get_stock_day('00001','2017-01-01','2017-01-31')
-# 前复权
-# data=QA.QAFetch.QATdx.QA_fetch_get_stock_day('00001','2017-01-01','2017-01-31','01')
-
-# 分钟线
-
-# 1min
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','1min')
-# 5min
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','5min')
-# 15min
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','15min')
-
-
-# 除权除息
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_xdxr('00001')
-
-# 股票列表
-
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
-# 指数列表
-#data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('index')
-# 全部列表
+import sys
+QA.QA_util_log_info('stock_day')
+QA.QA_util_log_info('不复权')  
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_day('00001','2017-01-01','2017-01-31')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('前复权')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_day('00001','2017-01-01','2017-01-31','01')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('分钟线')
+QA.QA_util_log_info('1min')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','1min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('5min')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','5min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('15min')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_min('000001','2017-07-01','2017-08-01','15min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('除权除息')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_xdxr('00001')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('股票列表')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('stock')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('指数列表')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('index')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('全部列表')
 data=QA.QAFetch.QATdx.QA_fetch_get_stock_list('all')
-"""
-QA.QAFetch.QATdx.QA_fetch_get_stock_list()
-QA.QAFetch.QATdx.QA_fetch_get_index_day()
-QA.QAFetch.QATdx.QA_fetch_get_index_min()
-QA.QAFetch.QATdx.QA_fetch_get_stock_latest()
-QA.QAFetch.QATdx.QA_fetch_get_stock_realtime()
-"""
-print(data)
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('指数日线')
+data=QA.QAFetch.QATdx.QA_fetch_get_index_day('000001','2017-01-01','2017-09-01')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('指数分钟线')
+QA.QA_util_log_info('1min')
+data=QA.QAFetch.QATdx.QA_fetch_get_index_min('000001','2017-07-01','2017-08-01','1min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('5min')
+data=QA.QAFetch.QATdx.QA_fetch_get_index_min('000001','2017-07-01','2017-08-01','5min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('15min')
+data=QA.QAFetch.QATdx.QA_fetch_get_index_min('000001','2017-07-01','2017-08-01','15min')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('最后一次交易价格')
+QA.QA_util_log_info('参数为列表')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_latest(['000001','000002'])
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('参数为一只股票')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_latest('000001')
+QA.QA_util_log_info(data)
+QA.QA_util_log_info('实时价格')
+data=QA.QAFetch.QATdx.QA_fetch_get_stock_realtime(['000001','000002'])
+QA.QA_util_log_info(data)
