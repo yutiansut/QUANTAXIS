67dbb144794a4717befff4635761655b4ae3b9bd
yutiansut <yutiansut@qq.com>
2017-09-11 09:47:41 +0800

    # update and fix bug

--- QUANTAXIS_Webkit/web/src/App.vue
+++ QUANTAXIS_Webkit/web/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <mu-appbar title="Title">
-      <mu-flat-button class="quantaxislogo" href='https://github.com/yutiansut/quantaxis' color="white" style="-webkit-app-region: drag" disable slot="left" >QUANTAXIS</mu-flat-button>
+      <mu-flat-button class="quantaxislogo" href='https://github.com/yutiansut/quantaxis' color="white" style="-webkit-app-region: drag"
+        disable slot="left">QUANTAXIS</mu-flat-button>
       <mu-raised-button label="MENU" slot="right" @click="toggle()" />
       <mu-drawer right :open="open" @close="toggle()">
         <mu-appbar title="MENU" />
@@ -12,7 +13,7 @@
           <router-link to='/'>
             <mu-list-item title="HOME" />
           </router-link>
-  
+
           <router-link to='/start'>
             <mu-list-item title="START" />
           </router-link>
@@ -26,31 +27,33 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data() {
-    return {
-      open: false
-    }
-  },
-  methods: {
-    toggle() {
-      this.open = !this.open
+  export default {
+    name: 'app',
+    data() {
+      return {
+        open: false
+      }
+    },
+    methods: {
+      toggle() {
+        this.open = !this.open
+      }
     }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-.mu-flat-button-wrapper {
-  font-size: 1.8em;
-}
+  .mu-flat-button-wrapper {
+    font-size: 1.8em;
+  }
+
 </style>
