fe7c75ff7bd816c9b1067ddc7c9fc9191da10231
yutiansut <yutiansut@qq.com>
2017-09-15 17:18:42 +0800

    #zmq model text

--- QUANTAXIS/QARPC/QA_zmq_client.py
+++ QUANTAXIS/QARPC/QA_zmq_client.py
@@ -1,13 +1,14 @@
 from zmq import *
 import zmq
 
-class MyPull():
-    def __init__(self, content, addr, num):
+class QA_RPC_client():
+    def __init__(self, addr, num):
         assert num > 0 and num < 256
         self._socks = {}
+        self._ctx=zmq.Context()
         self._poller = zmq.Poller()
         for i in range(num):
-            sock = content.socket(zmq.PULL)
+            sock = self._ctx.socket(zmq.PULL)
             sock.connect(addr)
             self._poller.register(sock, zmq.POLLIN)
             self._socks[sock] = i
@@ -28,8 +29,5 @@ class MyPull():
         if flag: self.pull() or None
     
     
-ctx = zmq.Context()
 
-pl = MyPull(ctx, 'tcp://127.0.0.1:8002', 5)
- 
-pl.pull()
+
--- QUANTAXIS/QARPC/tx.py
+++ QUANTAXIS/QARPC/tx.py
@@ -1,11 +1,11 @@
  
-from .QA_zmq_server import QA_RPC_Sever
 
+from .QA_zmq_client import QA_RPC_client
 import datetime
 import random
 from concurrent.futures import ThreadPoolExecutor
 def ports(item):
-    QA_RPC_Sever('tcp://*:%s'%item).push(datetime.datetime.now())
+    QA_RPC_client('tcp://*:%s'%item,5).pull()
 executor = ThreadPoolExecutor(max_workers=150)
 
 res = {executor.submit(
