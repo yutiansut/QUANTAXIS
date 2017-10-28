54d9dd853d95d489e6ac948971faa96d77984c24
yutiansut <yutiansut@qq.com>
2017-10-28 21:32:22 +0800

    #

--- QUANTAXIS/QAUtil/QASql.py
+++ QUANTAXIS/QAUtil/QASql.py
@@ -26,16 +26,28 @@ import re
 import datetime
 import time
 import motor
+import asyncio
+from motor import motor_asyncio
+from motor.motor_asyncio import (AsyncIOMotorCollection, AsyncIOMotorClient, AsyncIOMotorDatabase,
+                                 AsyncIOMotorCommandCursor)
 from .QALogs import QA_util_log_info
 
 
-def QA_util_sql_mongo_setting(ip='127.0.0.1',port=27017):
+def QA_util_sql_mongo_setting(ip='127.0.0.1', port=27017):
     QA_sql_mongo_client = pymongo.MongoClient(ip, int(port))
-    QA_util_log_info('ip:' + str(ip) + '   port:' + str(port))
+    QA_util_log_info('ip:{},port{}'.format(str(ip),str(port)))
     return QA_sql_mongo_client
 
 # async
-def QA_util_sql_async_mongo_setting(ip='127.0.0.1',port=27017):
-    QA_sql_mongo_client = motor.MotorClient(ip, int(port))
-    QA_util_log_info('ip:' + str(ip) + '   port:' + str(port))
-    return QA_sql_mongo_client
+
+
+def QA_util_sql_async_mongo_setting(ip='127.0.0.1', port=27017):
+    QA_sql_async_mongo_client = AsyncIOMotorClient(ip, int(port))
+    QA_util_log_info('ip:{},port{}'.format(str(ip),str(port)))
+    return QA_sql_async_mongo_client
+
+
+if __name__=='__main__':
+    # test async_mongo
+    client=QA_util_sql_async_mongo_setting().quantaxis.stock_day
+    print(client)
