69860fca687a50bd3959d477130e4886811d34c7
yutiansut <yutiansut@qq.com>
2017-11-22 20:09:09 +0800

    #bug

--- QUANTAXIS/QAUtil/QABar.py
+++ QUANTAXIS/QAUtil/QABar.py
@@ -43,7 +43,7 @@ def QA_util_make_min_index(day, type_='1min'):
 
 def QA_util_make_hour_index(day, type_='1h'):
     if QA_util_if_trade(day) is True:
-        return pd.date_range(str(day) + '09:30:00', str(day) + ' 11:30:00', freq=type_, closed='right').append(
+        return pd.date_range(str(day) + ' 09:30:00', str(day) + ' 11:30:00', freq=type_, closed='right').append(
             pd.date_range(str(day) + ' 13:00:00', str(day) + ' 15:00:00', freq=type_, closed='right'))
     else:
         return pd.DataFrame(['No trade'])
