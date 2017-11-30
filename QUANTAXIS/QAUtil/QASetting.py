7bff38f05b588c9efb5d180cc124a8a83b810376
hardywu <hardy0wu@gmail.com>
2017-11-30 23:29:45 +0800

    fix(QASetting): correct default port number

--- QUANTAXIS/QAUtil/QASetting.py
+++ QUANTAXIS/QAUtil/QASetting.py
@@ -29,7 +29,7 @@ from QUANTAXIS.QAUtil import QA_util_log_info, QA_util_sql_mongo_setting
 class QA_Setting():
 
     QA_util_sql_mongo_ip = os.getenv('MONGO_IP') if os.getenv('MONGO_IP') else '127.0.0.1'
-    QA_util_sql_mongo_port = os.getenv('MONGO_PORT') if os.getenv('MONGO_PORT') else '127.0.0.1'
+    QA_util_sql_mongo_port = os.getenv('MONGO_PORT') if os.getenv('MONGO_PORT') else '27017'
     client = QA_util_sql_mongo_setting(
         QA_util_sql_mongo_ip, QA_util_sql_mongo_port)
 
