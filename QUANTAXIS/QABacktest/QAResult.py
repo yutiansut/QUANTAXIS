2347b58ef05f7236b889a4c884e9d52f182b2eab
yutiansut <yutiansut@qq.com>
2017-12-01 17:00:32 +0800

    #commit 回测结果分析器

--- QUANTAXIS/QABacktest/QAResult.py
+++ QUANTAXIS/QABacktest/QAResult.py
@@ -24,29 +24,29 @@
 
 
 from QUANTAXIS.QAFetch.QAQuery import QA_fetch_backtest_history, QA_fetch_backtest_info
+from QUANTAXIS.QAUtil.QADate_trade import QA_util_date_gap
 import pandas as pd
 from statistics import mean
-
-
 
 
 class backtest_result_analyzer():
     def __init__(self, cookie_id, *args, **kwargs):
         self.cookie = cookie_id
-        self.backtest_history=QA_fetch_backtest_history(cookie=self.cookie)
-        self.backtest_info=QA_fetch_backtest_info(account_cookie=self.cookie)
+        self.backtest_history = QA_fetch_backtest_history(cookie=self.cookie)
+        self.backtest_info = QA_fetch_backtest_info(account_cookie=self.cookie)
 
     @property
     def history(self):
-        data=pd.DataFrame(self.backtest_history[0]['history'],columns=['datetime','code','price','towards','amounts','o_id','d_id','commission'])
-        return data.drop(['o_id','d_id'],axis=1)
-    
+        data = pd.DataFrame(self.backtest_history[0]['history'], columns=[
+                            'datetime', 'code', 'price', 'towards', 'amounts', 'o_id', 'd_id', 'commission'])
+        return data.drop(['o_id', 'd_id'], axis=1)
+
     @property
     def detail(self):
-        detail=pd.DataFrame(self.backtest_history[0]['detail'],columns=['date', 'code', 'price', 'amounts', 'order_id',
-                                                                                  'trade_id', 'sell_price', 'sell_order_id',
-                                                                                  'sell_trade_id', 'sell_date', 'left_amount',
-                                                                                  'commission'])
+        detail = pd.DataFrame(self.backtest_history[0]['detail'], columns=['date', 'code', 'price', 'amounts', 'order_id',
+                                                                           'trade_id', 'sell_price', 'sell_order_id',
+                                                                           'sell_trade_id', 'sell_date', 'left_amount',
+                                                                           'commission'])
 
         detail['sell_average'] = detail['sell_price'].apply(lambda x: _mean(x))
 
@@ -57,28 +57,32 @@ class backtest_result_analyzer():
             detail['pnl'] = detail['pnl_price'] * (
                 detail['amounts'] - detail['left_amount']) - detail['commission']
 
-
-            detail['pnl_precentage']=detail['pnl_price']/detail['price']
+            detail['pnl_precentage'] = detail['pnl_price'] / detail['price']
         except:
             pass
 
         return detail.drop(
             ['order_id', 'trade_id', 'sell_order_id', 'sell_trade_id'], axis=1)
 
-        
     @property
     def codes(self):
         return self.history.code.unique().tolist()
-    
-    def get_stock_tradehistory(self,code):
-        return self.history[self.history.code==code]
-    def get_stock_tradedetail(self,code):
-        return self.detail[self.detail.code==code]
-    
-    def get_loss_trade(self,num=5):
-        return self.detail[self.detail.pnl_precentage<=0].sort_values(by=['pnl_precentage'],ascending=True).head(num)
-    def get_profit_trade(self,num=5):
-        return self.detail[self.detail.pnl_precentage>=0].sort_values(by=['pnl_precentage'],ascending=False).head(num)
+
+    def get_stock_tradehistory(self, code):
+        return self.history[self.history.code == code]
+
+    def get_stock_tradedetail(self, code):
+        return self.detail[self.detail.code == code]
+
+    def get_loss_trade(self, num=5):
+        return self.detail[self.detail.pnl_precentage <= 0].sort_values(by=['pnl_precentage'], ascending=True).head(num)
+
+    def get_profit_trade(self, num=5):
+        return self.detail[self.detail.pnl_precentage >= 0].sort_values(by=['pnl_precentage'], ascending=False).head(num)
+
+    def get_trade_marketdata(self, rx):
+        return QA.QA_fetch_stock_day_adv(rx.code.values[0], QA_util_date_gap(rx.date.values[0], gap=3, methods='lt'), QA_util_date_gap(rx.sell_date.values[0][-1], gap=3, methods='gt'))
+
 
 def _mean(list_):
     if len(list_) > 0:
@@ -87,12 +91,10 @@ def _mean(list_):
         return 'No Data'
 
 
-
-
-if __name__=='__main__':
-    ana=backtest_result_analyzer(cookie_id='0.0792467630583924')
+if __name__ == '__main__':
+    ana = backtest_result_analyzer(cookie_id='0.0792467630583924')
     print(ana.detail)
-    code=ana.codes
+    code = ana.codes
     print(ana.get_stock_tradehistory(code[1]))
     print(ana.get_stock_tradedetail(code[1]))
     print(ana.get_loss_trade())
