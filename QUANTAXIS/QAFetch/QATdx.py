a31cd565ed25beea4a457fa60a29b3175ea377ed
yutiansut <yutiansut@qq.com>
2017-12-04 12:49:00 +0800

    # 试图对于实时存储做出一点更新

--- QUANTAXIS/QAFetch/QATdx.py
+++ QUANTAXIS/QAFetch/QATdx.py
@@ -348,7 +348,7 @@ def QA_fetch_get_stock_realtime(code=['000001', '000002'], ip=best_ip, port=7709
             __data = __data.append(api.to_df(api.get_security_quotes(
                 [(__select_market_code(x), x) for x in code[80 * id_:80 * (id_ + 1)]])))
             __data['datetime'] = datetime.datetime.now()
-        data = __data[['datetime', 'last_close', 'code', 'open', 'high', 'low', 'price', 'cur_vol',
+        data = __data[['datetime', 'active1','active2','last_close', 'code', 'open', 'high', 'low', 'price', 'cur_vol',
                        's_vol', 'b_vol', 'vol', 'ask1', 'ask_vol1', 'bid1', 'bid_vol1', 'ask2', 'ask_vol2',
                        'bid2', 'bid_vol2', 'ask3', 'ask_vol3', 'bid3', 'bid_vol3', 'ask4',
                        'ask_vol4', 'bid4', 'bid_vol4', 'ask5', 'ask_vol5', 'bid5', 'bid_vol5']]
