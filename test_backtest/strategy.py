4bfc831c5aac86078ff46e505f7bd2dfaaca65c2
yutiansut <yutiansut@qq.com>
2018-01-12 12:59:48 +0800

    #

--- test_backtest/strategy.py
+++ test_backtest/strategy.py
@@ -10,6 +10,42 @@ class MAStrategy(QA_Strategy):
         super().__init__()
 
     def on_bar(self, event):
+        try:
+            for item in event.market_data.code:
+                if self.sell_available is None:
+                    print(self.current_time)
+                    
+                    event.send_order(account_id=self.account_cookie,
+                                     amount=10000, amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                     time=self.current_time, code=item, price=0,
+                                     order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.BUY,
+                                     market_type=MARKET_TYPE.STOCK_DAY, data_type=MARKETDATA_TYPE.DAY,
+                                     broker_name=self.broker)
+
+                else:
+                    if self.sell_available.get(item, 0) > 0:
+                        event.send_order(account_id=self.account_cookie,
+                                         amount=self.sell_available[item], amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                         time=self.current_time, code=item, price=0,
+                                         order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.SELL,
+                                         market_type=MARKET_TYPE.STOCK_DAY, data_type=MARKETDATA_TYPE.DAY,
+                                         broker_name=self.broker
+                                         )
+                    else:
+                        event.send_order(account_id=self.account_cookie,
+                                         amount=10000, amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                         time=self.current_time, code=item, price=0,
+                                         order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.BUY,
+                                         market_type=MARKET_TYPE.STOCK_DAY, data_type=MARKETDATA_TYPE.DAY,
+                                         broker_name=self.broker)
+        except:
+            pass
+
+class DUOStrategy(QA_Strategy):
+    def __init__(self):
+        super().__init__()
+
+    def on_bar(self, event):
         if self.market_data.len > 1:
             for item in event.market_data.code:
                 if self.sell_available is None:
