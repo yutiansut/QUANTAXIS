aaaef5350012e0df566842d937038aee3fc46eb3
yutiansut <yutiansut@qq.com>
2018-02-03 12:02:52 +0800

    #update func doc

--- QUANTAXIS/QAUtil/QACfg.py
+++ QUANTAXIS/QAUtil/QACfg.py
@@ -27,11 +27,26 @@ from configparser import ConfigParser
 
 
 def QA_util_cfg_initial(CONFIG_FILE):
+    """[summary]
+    
+    Arguments:
+        CONFIG_FILE {[type]} -- [description]
+    """
 
     pass
 
 
 def QA_util_get_cfg(__file_path, __file_name):
+    """[summary]
+    
+    Arguments:
+        __file_path {[type]} -- [description]
+        __file_name {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
     __setting_file = ConfigParser()
     try:
         return __setting_file.read(__file_path + __file_name)
