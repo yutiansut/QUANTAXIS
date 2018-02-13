80b7e08f364a337e6774cd5cb2260648c521a9bc
yutiansut <yutiansut@qq.com>
2018-02-13 10:43:33 +0800

    #update documents

--- QUANTAXIS/QASU/main.py
+++ QUANTAXIS/QASU/main.py
@@ -30,56 +30,155 @@ from QUANTAXIS.QAUtil import DATABASE
 
 
 def QA_SU_save_stock_info(engine, client=DATABASE):
+    """save stock info
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_info(client=client)
 
 
 def QA_SU_save_stock_list(engine, client=DATABASE):
+    """save stock_list
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_list(client=client)
 
 
 def QA_SU_save_stock_day(engine, client=DATABASE):
+    """save stock_day
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_day(client=client)
 
 
 def QA_SU_save_stock_min(engine, client=DATABASE):
+    """save stock_min
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_min(client=client)
 
 
 def QA_SU_save_index_day(engine, client=DATABASE):
+    """save index_day
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_index_day(client=client)
 
 
 def QA_SU_save_index_min(engine, client=DATABASE):
+    """save index_min
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_index_min(client=client)
 
 
 def QA_SU_save_etf_day(engine, client=DATABASE):
+    """save etf_day
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_etf_day(client=client)
 
 
 def QA_SU_save_etf_min(engine, client=DATABASE):
+    """save etf_min
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_etf_min(client=client)
 
 
 def QA_SU_save_stock_xdxr(engine, client=DATABASE):
+    """save stock_xdxr
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_xdxr(client=client)
 
 def QA_SU_save_stock_block(engine, client=DATABASE):
+    """save stock_block
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
     engine = select_save_engine(engine)
     engine.QA_SU_save_stock_block(client=client)
 
 
 
 def select_save_engine(engine):
+    """select save_engine
+    
+    Arguments:
+        engine {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
 
     if engine in ['tushare', 'ts', 'Tushare']:
         return sts
@@ -90,4 +189,16 @@ def select_save_engine(engine):
 
 
 def QA_SU_save_stock_min_5(file_dir, client=DATABASE):
+    """save stock_min5
+    
+    Arguments:
+        file_dir {[type]} -- [description]
+    
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    
+    Returns:
+        [type] -- [description]
+    """
+
     return tdx_file.QA_save_tdx_to_mongo(file_dir, client)
