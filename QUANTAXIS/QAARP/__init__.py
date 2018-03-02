0d2343f5e9d351c45ff6523b1c534f7b1d95699a
yutiansut <yutiansut@qq.com>
2018-03-02 19:33:03 +0800

    #arp update for risk.portfolio

--- QUANTAXIS/QAARP/__init__.py
+++ QUANTAXIS/QAARP/__init__.py
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 from QUANTAXIS.QAARP.QAAccount import QA_Account
-from QUANTAXIS.QAARP.QARisk import QA_Risk, QA_Performace
+from QUANTAXIS.QAARP.QARisk import QA_Risk, QA_Performance
 from QUANTAXIS.QAARP.QAPortfolio import QA_Portfolio
 from QUANTAXIS.QAARP.QAUser import QA_User
 from QUANTAXIS.QAARP.QAStrategy import QA_Strategy
