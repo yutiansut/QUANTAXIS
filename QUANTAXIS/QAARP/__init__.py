c9aa4ac0fd7799773eccb410cecb70aeff809353
yutiansut <yutiansut@qq.com>
2018-03-03 21:58:11 +0800

    #update

--- QUANTAXIS/QAARP/__init__.py
+++ QUANTAXIS/QAARP/__init__.py
@@ -24,8 +24,6 @@
 
 from QUANTAXIS.QAARP.QAAccount import QA_Account
 from QUANTAXIS.QAARP.QARisk import QA_Risk, QA_Performance
-from QUANTAXIS.QAARP.QAPortfolio import QA_Portfolio
+from QUANTAXIS.QAARP.QAPortfolio import QA_Portfolio, QA_PortfolioView
 from QUANTAXIS.QAARP.QAUser import QA_User
 from QUANTAXIS.QAARP.QAStrategy import QA_Strategy
-
-
