c7955f33895ff58a0936af71524d206818c16e5f
yutiansut <yutiansut@qq.com>
2018-03-08 13:30:29 +0800

    #update

--- QUANTAXIS/QAFetch/QATdx_adv.py
+++ QUANTAXIS/QAFetch/QATdx_adv.py
@@ -33,7 +33,7 @@ import pandas as pd
 from pytdx.hq import TdxHq_API
 
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_tradetime
-from QUANTAXIS.QAUtil.QASetting import DATABASE, info_ip_list
+from QUANTAXIS.QAUtil.QASetting import DATABASE, stock_ip_list
 from QUANTAXIS.QAUtil.QASql import QA_util_sql_mongo_sort_ASCENDING
 from QUANTAXIS.QAUtil.QATransform import QA_util_to_json_from_pandas
 
@@ -133,7 +133,7 @@ class QA_Tdx_Executor():
     def api_worker(self):
         data = []
         if self._queue.qsize() < 80:
-            for item in info_ip_list:
+            for item in stock_ip_list:
                 _sec = self._test_speed(item)
                 if _sec < 0.1:
                     try:
