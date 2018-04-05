0b28c47be6f501ab17164ee4109d73548ea8c44d
yutiansut <yutiansut@qq.com>
2018-04-05 18:50:46 +0800

    #update analysis trade

--- QUANTAXIS/QAAnalysis/QAAnalysis_trade.py
+++ QUANTAXIS/QAAnalysis/QAAnalysis_trade.py
@@ -27,68 +27,69 @@ from statistics import mean
 
 import pandas as pd
 
+from QUANTAXIS.QAARP.QAAccount import QA_Account
+from QUANTAXIS.QAARP.QARisk import QA_Performance, QA_Risk
+from QUANTAXIS.QAEngine.QAEvent import QA_Event
 from QUANTAXIS.QAFetch.QAQuery import (QA_fetch_backtest_history,
                                        QA_fetch_backtest_info)
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
-from QUANTAXIS.QAUtil.QADate_trade import QA_util_date_gap
+from QUANTAXIS.QAMarket.QABacktestBroker import QA_BacktestBroker
+from QUANTAXIS.QAUtil.QADate_trade import (QA_util_date_gap,
+                                           QA_util_get_next_day)
+from QUANTAXIS.QAUtil.QAParameter import (AMOUNT_MODEL, ORDER_DIRECTION,
+                                          ORDER_MODEL)
 
 
 class QAAnalysis_trade():
-    def __init__(self, cookie_id, *args, **kwargs):
-        self.cookie = cookie_id
-        self.backtest_history = QA_fetch_backtest_history(cookie=self.cookie)
-        self.backtest_info = QA_fetch_backtest_info(account_cookie=self.cookie)
 
-    @property
-    def history(self):
-        data = pd.DataFrame(self.backtest_history[0]['history'], columns=[
-                            'datetime', 'code', 'price', 'towards', 'amounts', 'o_id', 'd_id', 'commission'])
-        return data.drop(['o_id', 'd_id'], axis=1)
+    """
+    Account/Portfolio是一个标准单元,所有成交记录分析 都会被加载到该单元中进行分析
+    当我们只有一个成交记录的时候,我们会创建一个账户单元
+    """
 
-    @property
-    def detail(self):
-        detail = pd.DataFrame(self.backtest_history[0]['detail'], columns=['date', 'code', 'price', 'amounts', 'order_id',
-                                                                           'trade_id', 'sell_price', 'sell_order_id',
-                                                                           'sell_trade_id', 'sell_date', 'left_amount',
-                                                                           'commission'])
+    def __init__(self, init_assets, *args, **kwargs):
+        self.account = QA_Account(init_assets=init_assets)
+        self.backtest_broker = QA_BacktestBroker()
 
-        detail['sell_average'] = detail['sell_price'].apply(lambda x: _mean(x))
+    def import_trade(self, trade):
+        """
+        trade是一个可迭代的list/generator
+        """
+        for item in trade:
+            self.make_deal(item.code, item.datetime, item.amount,
+                           item.towards, item.price.item.order_model, item.amount_model)
 
-        try:
-            detail['pnl_price'] = detail['sell_average'] - \
-                detail['price']
+    def make_deal(self, code, datetime, amount, towards, price, order_model=ORDER_MODEL.MARKET, amount_model=AMOUNT_MODEL.BY_AMOUNT):
+        """
+        这是一个一定会成交,并且立刻结转(及t+0)的交易入口
+        """
+        self.account.receive_deal(self.backtest_broker.receive_order(QA_Event(order=self.account.send_order(
+            code=code, time=datetime, amount=amount, towards=towards, price=price, order_model=order_model, amount_model=amount_model
+        ))))
+        self.account.settle()
 
-            detail['pnl'] = detail['pnl_price'] * (
-                detail['amounts'] - detail['left_amount']) - detail['commission']
 
-            detail['pnl_precentage'] = detail['pnl_price'] / detail['price']
-        except:
-            pass
+    # @property
+    # def codes(self):
+    #     return self.code
 
-        return detail.drop(
-            ['order_id', 'trade_id', 'sell_order_id', 'sell_trade_id'], axis=1)
+    # def get_stock_tradehistory(self, code):
+    #     return self.history.query('code=="{}"'.format(code))
 
-    @property
-    def codes(self):
-        return self.history.code.unique().tolist()
+    # def get_stock_tradedetail(self, code):
+    #     return self.detail.query('code=="{}"'.format(code))
 
-    def get_stock_tradehistory(self, code):
-        return self.history.query('code=="{}"'.format(code))
+    # def get_loss_trade(self, num=5):
+    #     return self.detail[self.detail.pnl_precentage <= 0].sort_values(by=['pnl_precentage'], ascending=True).head(num)
 
-    def get_stock_tradedetail(self, code):
-        return self.detail.query('code=="{}"'.format(code))
+    # def get_profit_trade(self, num=5):
+    #     return self.detail[self.detail.pnl_precentage >= 0].sort_values(by=['pnl_precentage'], ascending=False).head(num)
 
-    def get_loss_trade(self, num=5):
-        return self.detail[self.detail.pnl_precentage <= 0].sort_values(by=['pnl_precentage'], ascending=True).head(num)
+    # def get_trade_marketdata(self, rx, gap=3):
+    #     return QA_fetch_stock_day_adv(rx.code.values[0], QA_util_date_gap(rx.date.values[0], gap, methods='lt'), QA_util_date_gap(rx.sell_date.values[0][-1], gap, methods='gt'))
 
-    def get_profit_trade(self, num=5):
-        return self.detail[self.detail.pnl_precentage >= 0].sort_values(by=['pnl_precentage'], ascending=False).head(num)
-
-    def get_trade_marketdata(self, rx, gap=3):
-        return QA_fetch_stock_day_adv(rx.code.values[0], QA_util_date_gap(rx.date.values[0], gap, methods='lt'), QA_util_date_gap(rx.sell_date.values[0][-1], gap, methods='gt'))
-
-    def get_trade_before_and_after_pnl(self, rx, N=3, M=10):
-        data = self.get_trade_marketdata(rx, M)
+    # def get_trade_before_and_after_pnl(self, rx, N=3, M=10):
+    #     data = self.get_trade_marketdata(rx, M)
 
 
 def _mean(list_):
@@ -96,13 +97,3 @@ def _mean(list_):
         return mean(list_)
     else:
         return 'No Data'
-
-
-if __name__ == '__main__':
-    ana = backtest_result_analyzer(cookie_id='0.0792467630583924')
-    print(ana.detail)
-    code = ana.codes
-    print(ana.get_stock_tradehistory(code[1]))
-    print(ana.get_stock_tradedetail(code[1]))
-    print(ana.get_loss_trade())
-    print(ana.get_profit_trade())
