3a85327ac0fb054262723923510097d364027331
yutiansut <yutiansut@qq.com>
2018-05-08 17:50:07 +0800

    #update for save stock_month/year

--- QUANTAXIS/QASU/save_tdx.py
+++ QUANTAXIS/QASU/save_tdx.py
@@ -106,7 +106,166 @@ def QA_SU_save_stock_day(client=DATABASE):
         QA_util_log_info('ERROR CODE \n ')
         QA_util_log_info(err)
 
+def QA_SU_save_stock_week(client=DATABASE):
+    """save stock_week
 
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
+    stock_list = QA_fetch_get_stock_time_to_market()
+    coll_stock_week = client.stock_week
+    coll_stock_week.create_index(
+        [("code", pymongo.ASCENDING), ("date_stamp", pymongo.ASCENDING)])
+    err = []
+
+    def __saving_work(code, coll_stock_week):
+        try:
+            QA_util_log_info(
+                '##JOB01 Now Saving STOCK_WEEK==== %s' % (str(code)))
+
+            ref = coll_stock_week.find({'code': str(code)[0:6]})
+            end_date = str(now_time())[0:10]
+            if ref.count() > 0:
+                    # 加入这个判断的原因是因为如果股票是刚上市的 数据库会没有数据 所以会有负索引问题出现
+
+                start_date = ref[ref.count() - 1]['date']
+
+                QA_util_log_info(' UPDATE_STOCK_WEEK \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_week.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), QA_util_get_next_day(start_date), end_date, '00',frequence='week')))
+            else:
+                start_date = '1990-01-01'
+                QA_util_log_info(' UPDATE_STOCK_WEEK \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_week.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), start_date, end_date, '00',frequence='week')))
+        except:
+            err.append(str(code))
+    for item in range(len(stock_list)):
+        QA_util_log_info('The %s of Total %s' %
+                         (item, len(stock_list)))
+        QA_util_log_info('DOWNLOAD PROGRESS %s ' % str(
+            float(item / len(stock_list) * 100))[0:4] + '%')
+
+        __saving_work(stock_list.index[item], coll_stock_week)
+    if len(err) < 1:
+        QA_util_log_info('SUCCESS')
+    else:
+        QA_util_log_info('ERROR CODE \n ')
+        QA_util_log_info(err)
+def QA_SU_save_stock_month(client=DATABASE):
+    """save stock_month
+
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
+    stock_list = QA_fetch_get_stock_time_to_market()
+    coll_stock_month = client.stock_month
+    coll_stock_month.create_index(
+        [("code", pymongo.ASCENDING), ("date_stamp", pymongo.ASCENDING)])
+    err = []
+
+    def __saving_work(code, coll_stock_month):
+        try:
+            QA_util_log_info(
+                '##JOB01 Now Saving STOCK_MONTH==== %s' % (str(code)))
+
+            ref = coll_stock_month.find({'code': str(code)[0:6]})
+            end_date = str(now_time())[0:10]
+            if ref.count() > 0:
+                    # 加入这个判断的原因是因为如果股票是刚上市的 数据库会没有数据 所以会有负索引问题出现
+
+                start_date = ref[ref.count() - 1]['date']
+
+                QA_util_log_info(' UPDATE_STOCK_MONTH \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_month.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), QA_util_get_next_day(start_date), end_date, '00',frequence='month')))
+            else:
+                start_date = '1990-01-01'
+                QA_util_log_info(' UPDATE_STOCK_MONTH \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_month.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), start_date, end_date, '00',frequence='month')))
+        except:
+            err.append(str(code))
+    for item in range(len(stock_list)):
+        QA_util_log_info('The %s of Total %s' %
+                         (item, len(stock_list)))
+        QA_util_log_info('DOWNLOAD PROGRESS %s ' % str(
+            float(item / len(stock_list) * 100))[0:4] + '%')
+
+        __saving_work(stock_list.index[item], coll_stock_month)
+    if len(err) < 1:
+        QA_util_log_info('SUCCESS')
+    else:
+        QA_util_log_info('ERROR CODE \n ')
+        QA_util_log_info(err)
+
+def QA_SU_save_stock_year(client=DATABASE):
+    """save stock_year
+
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
+    stock_list = QA_fetch_get_stock_time_to_market()
+    coll_stock_year = client.stock_year
+    coll_stock_year.create_index(
+        [("code", pymongo.ASCENDING), ("date_stamp", pymongo.ASCENDING)])
+    err = []
+
+    def __saving_work(code, coll_stock_year):
+        try:
+            QA_util_log_info(
+                '##JOB01 Now Saving STOCK_YEAR==== %s' % (str(code)))
+
+            ref = coll_stock_year.find({'code': str(code)[0:6]})
+            end_date = str(now_time())[0:10]
+            if ref.count() > 0:
+                    # 加入这个判断的原因是因为如果股票是刚上市的 数据库会没有数据 所以会有负索引问题出现
+
+                start_date = ref[ref.count() - 1]['date']
+
+                QA_util_log_info(' UPDATE_STOCK_YEAR \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_year.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), QA_util_get_next_day(start_date), end_date, '00',frequence='year')))
+            else:
+                start_date = '1990-01-01'
+                QA_util_log_info(' UPDATE_STOCK_YEAR \n Trying updating %s from %s to %s' %
+                                 (code, start_date, end_date))
+                if start_date != end_date:
+                    coll_stock_year.insert_many(
+                        QA_util_to_json_from_pandas(
+                            QA_fetch_get_stock_day(str(code), start_date, end_date, '00',frequence='year')))
+        except:
+            err.append(str(code))
+    for item in range(len(stock_list)):
+        QA_util_log_info('The %s of Total %s' %
+                         (item, len(stock_list)))
+        QA_util_log_info('DOWNLOAD PROGRESS %s ' % str(
+            float(item / len(stock_list) * 100))[0:4] + '%')
+
+        __saving_work(stock_list.index[item], coll_stock_year)
+    if len(err) < 1:
+        QA_util_log_info('SUCCESS')
+    else:
+        QA_util_log_info('ERROR CODE \n ')
+        QA_util_log_info(err)
 def QA_SU_save_stock_xdxr(client=DATABASE):
     """[summary]
 
