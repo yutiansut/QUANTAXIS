af3878b1688c6fb69ec0d9ab817998783536da4b
JerryW <wang_xiao_zhen@hotmail.com>
2018-05-15 20:03:46 +0800

    添加单元测试 通达信 和 http://www.wstock.net/ 分析家 日线数据对比。 成交量 和 成交金额 有误差，有待研究 ，

--- QUANTAXIS_Test/QAUtil_Test/QADate_trade_Test.py
+++ QUANTAXIS_Test/QAUtil_Test/QADate_trade_Test.py
@@ -6,7 +6,7 @@ from QUANTAXIS.QAFetch import (QATdx );
 from QUANTAXIS.QAUtil import (QADate, QADate_trade );
 
 
-class Test_QA_Fetch(unittest.TestCase):
+class Test_QA_Date_trade(unittest.TestCase):
     def test_QA_util_if_trade(self):
 
         now = QADate.QA_util_time_now()
