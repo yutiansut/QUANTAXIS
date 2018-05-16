e0b30687ff97f0fbffb4513d50895641346703b7
JerryW <wang_xiao_zhen@hotmail.com>
2018-05-16 19:40:14 +0800

    修复了 bug，  QAUser 中的 cookie 的问题

--- QUANTAXIS_Test/QABacktest_Test/QABacktest_Test.py
+++ QUANTAXIS_Test/QABacktest_Test/QABacktest_Test.py
@@ -18,6 +18,54 @@ from QUANTAXIS.QAUtil.QAParameter import (AMOUNT_MODEL, BROKER_EVENT,
 
 from QUANTAXIS.QABacktest.QABacktest import (QA_Backtest)
 
+
+from QUANTAXIS.QAARP.QAStrategy import QA_Strategy
+from QUANTAXIS.QAUtil.QAParameter import (AMOUNT_MODEL, MARKET_TYPE,
+                                          FREQUENCE, ORDER_DIRECTION,
+                                          ORDER_MODEL)
+from QUANTAXIS.QAUtil.QALogs import QA_util_log_info
+
+class MAStrategy(QA_Strategy):
+    def __init__(self):
+        super().__init__()
+        self.frequence = FREQUENCE.DAY
+        self.market_type = MARKET_TYPE.STOCK_CN
+
+    def on_bar(self, event):
+        sellavailable=self.sell_available
+        try:
+            for item in event.market_data.code:
+                if sellavailable is None:
+
+                    event.send_order(account_id=self.account_cookie,
+                                     amount=100, amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                     time=self.current_time, code=item, price=0,
+                                     order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.BUY,
+                                     market_type=self.market_type, frequence=self.frequence,
+                                     broker_name=self.broker)
+
+                else:
+                    if sellavailable.get(item, 0) > 0:
+                        event.send_order(account_id=self.account_cookie,
+                                         amount=sellavailable[item], amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                         time=self.current_time, code=item, price=0,
+                                         order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.SELL,
+                                         market_type=self.market_type, frequence=self.frequence,
+                                         broker_name=self.broker
+                                         )
+                    else:
+                        event.send_order(account_id=self.account_cookie,
+                                         amount=100, amount_model=AMOUNT_MODEL.BY_AMOUNT,
+                                         time=self.current_time, code=item, price=0,
+                                         order_model=ORDER_MODEL.MARKET, towards=ORDER_DIRECTION.BUY,
+                                         market_type=self.market_type, frequence=self.frequence,
+                                         broker_name=self.broker)
+
+        except:
+            pass
+
+
+
 class Test_QABacktest(unittest.TestCase):
 
     def testBacktraceTest(self):
@@ -27,6 +75,7 @@ class Test_QABacktest(unittest.TestCase):
                                end='2017-1-30',
                                code_list=['000001', '600010'],
                                commission_fee=0.00015)
+        #backtest.
         backtest._generate_account()
         backtest.start_market()
         backtest.run()
