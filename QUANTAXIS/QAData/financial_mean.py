39961d7932c9c02aa1a4254552c38a46648c9030
yutiansut <yutiansut@qq.com>
2018-05-28 23:17:50 +0800

    #update financial_mean

--- QUANTAXIS/QAData/financial_mean.py
+++ QUANTAXIS/QAData/financial_mean.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
 financial_dict = {
+    ## 每股指标
     '基本每股收益': 'EPS',
     '扣除非经常性损益每股收益': 'deductEPS',
     '每股未分配利润': 'undistributedProfitPerShare',
@@ -8,6 +9,7 @@ financial_dict = {
     '每股资本公积金': 'capitalReservePerShare',
     '净资产收益率': 'ROE',
     '每股经营现金流量': 'operatingCashFlowPerShare',
+    ## 资产负债表
     '货币资金': 'moneyFunds',
     '交易性金融资产': 'tradingFinancialAssets',
     '应收票据': 'billsReceivables',
@@ -74,6 +76,7 @@ financial_dict = {
     '非正常经营项目收益调整': 'abnormalBusinessProjectEarningsAdjustment',
     '所有者权益（或股东权益）合计': 'totalOwnersEquity',
     '负债和所有者（或股东权益）合计': 'totalLiabilitiesAndOwnersEquity',
+    ## 利润表
     '其中：营业收入': 'operatingRevenue',
     '其中：营业成本': 'operatingCosts',
     '营业税金及附加': 'taxAndSurcharges',
@@ -98,42 +101,50 @@ financial_dict = {
     '五、净利润': 'netProfit',
     '归属于母公司所有者的净利润': 'netProfitsBelongToParentCompanyOwner',
     '少数股东损益': 'minorityProfitAndLoss',
+
+    ## 现金流量表
+    ### 经营活动 Operating
     '销售商品、提供劳务收到的现金': 'cashFromGoodsSalesAndServicesProvided',
     '收到的税费返还': 'refundOfTaxAndFeeReceived',
     '收到其他与经营活动有关的现金': 'otherCashRelatedBusinessActivitiesReceived',
-    '经营活动现金流入小计': 'subtotalOfCashFlowFromOperatingActivities',
+    '经营活动现金流入小计': 'cashInflowsFromOperatingActivities',
     '购买商品、接受劳务支付的现金': 'buyingGoodsReceivingCashPaidForLabor',
     '支付给职工以及为职工支付的现金': 'paymentToEmployeesAndCashPaidForEmployees',
     '支付的各项税费': 'paymentsOfVariousTaxes',
     '支付其他与经营活动有关的现金': 'paymentOfOtherCashRelatedToBusinessActivities',
-    '经营活动现金流出小计': 'subtotalOfCashFlowFromOperatingActivities',
-    '经营活动产生的现金流量净额': 'netCashFlowFromOperatingActivities',
+    '经营活动现金流出小计': 'cashOutflowsFromOperatingActivities',
+    '经营活动产生的现金流量净额': 'netCashFlowsFromOperatingActivities',
+    ### 投资活动 Investment
     '收回投资收到的现金': 'cashReceivedFromInvestmentReceived',
     '取得投资收益收到的现金': 'cashReceivedFromInvestmentIncome',
     '处置固定资产、无形资产和其他长期资产收回的现金净额': 'disposalOfNetCashForRecoveryOfFixedAssetsIntangibleAssetsAndOtherLongTermAssets',
     '处置子公司及其他营业单位收到的现金净额': 'disposalOfNetCashReceivedFromSubsidiariesAndOtherBusinessUnits',
     '收到其他与投资活动有关的现金': 'otherCashReceivedRelatingToInvestingActivities',
-    '投资活动现金流入小计': 'subtotalOfCashFlowFromInvestmentActivities',
+    '投资活动现金流入小计': 'cashinFlowsFromInvestmentActivities',
     '购建固定资产、无形资产和其他长期资产支付的现金': 'cashForThePurchaseConstructionPaymentOfFixedAssetsIntangibleAssetsAndOtherLongTermAssets',
     '投资支付的现金': 'cashInvestment',
     '取得子公司及其他营业单位支付的现金净额': 'acquisitionOfNetCashPaidBySubsidiariesAndOtherBusinessUnits',
     '支付其他与投资活动有关的现金': 'otherCashPaidRelatingToInvestingActivities',
-    '投资活动现金流出小计': 'subtotalOfCashFlowFromInvestmentActivities',
+    '投资活动现金流出小计': 'cashOutflowsFromInvestmentActivities',
     '投资活动产生的现金流量净额':'netCashFlowsFromInvestingActivities',
-    '吸收投资收到的现金':'',
-    '取得借款收到的现金':'',
-    '收到其他与筹资活动有关的现金':'',
-    '筹资活动现金流入小计':'',
-    '偿还债务支付的现金':'',
-    '分配股利、利润或偿付利息支付的现金':'',
-    '支付其他与筹资活动有关的现金':'',
-    '筹资活动现金流出小计':'',
-    '筹资活动产生的现金流量净额':'',
-    '四、汇率变动对现金的影响':'',
-    '四(2)、其他原因对现金的影响':'',
-    '五、现金及现金等价物净增加额':'',
-    '期初现金及现金等价物余额':'',
-    '期末现金及现金等价物余额':'',
+    ### 筹资活动 Financing
+    '吸收投资收到的现金':'cashReceivedFromInvestors',
+    '取得借款收到的现金':'cashFromBorrowings',
+    '收到其他与筹资活动有关的现金':'otherCashReceivedRelatingToFinancingActivities',
+    '筹资活动现金流入小计':'cashInflowsFromFinancingActivities',
+    '偿还债务支付的现金':'cashPaymentsOfAmountBorrowed',
+    '分配股利、利润或偿付利息支付的现金':'cashPaymentsForDistrbutionOfDividendsOrProfits',
+    '支付其他与筹资活动有关的现金':'otherCashPaymentRelatingToFinancingActivities',
+    '筹资活动现金流出小计':'cashOutflowsFromFinancingActivities',
+    '筹资活动产生的现金流量净额':'netCashFlowsFromFinancingActivities',
+    ### 汇率变动
+    '四、汇率变动对现金的影响':'effectOfForeignExchangRateChangesOnCash',
+    '四(2)、其他原因对现金的影响':'effectOfOtherReasonOnCash',
+    ### 现金及现金等价物净增加
+    '五、现金及现金等价物净增加额':'netIncreaseInCashAndCashEquivalents',
+    '期初现金及现金等价物余额':'initialCashAndCashEquivalentsBalance',
+    ### 期末现金及现金等价物余额
+    '期末现金及现金等价物余额':'theFinalCashAndCashEquivalentsBalance',
     '净利润':'',
     '加：资产减值准备':'',
     '固定资产折旧、油气资产折耗、生产性生物资产折旧':'',
@@ -159,6 +170,7 @@ financial_dict = {
     '加：现金等价物的期末余额':'',
     '减：现金等价物的期初余额':'',
     '现金及现金等价物净增加额':'',
+    ## 偿债能力分析
     '流动比率':'',
     '速动比率':'',
     '现金比率(%)':'',
@@ -172,6 +184,7 @@ financial_dict = {
     '有形资产/负债合计(%)':'',
     '经营活动产生的现金流量净额/负债合计(%)':'',
     'EBITDA/负债合计(%)':'',
+    ## 经营效率分析
     '应收帐款周转率':'',
     '存货周转率':'',
     '运营资金周转率':'',
@@ -184,6 +197,7 @@ financial_dict = {
     '总资产周转天数':'',
     '股东权益周转率':'',
     '营业收入增长率(%)':'',
+    ## 发展能力分析
     '净利润增长率(%)':'',
     '净资产增长率(%)':'',
     '固定资产增长率(%)':'',
@@ -193,6 +207,7 @@ financial_dict = {
     '暂无':'',
     '暂无':'',
     '暂无':'',
+    ## 获利能力分析
     '成本费用利润率(%)':'',
     '营业利润率':'',
     '营业税金率':'',
@@ -210,4 +225,5 @@ financial_dict = {
     '息税前利润(EBIT)':'',
     '息税折旧摊销前利润(EBITDA)':'',
     'EBITDA/营业总收入(%)':''
+    ## 资本结构分析
 }
