f60147bd0004edd7b4745352c4a5d081435a200b
tauruswang <taurus.wang.xiao.zhen@gmail.com>
2018-05-29 22:06:21 +0800

    Revert "QARisk 输出 mssage 中输出浮点数 xxx.xx 'annualize_return': round(self.annualize_return,2), 'profit': round(self.profit,2),"
    This reverts commit 9e1ad57

--- QUANTAXIS_Test/QAUtil_Test/QATest_None.py
+++ QUANTAXIS_Test/QAUtil_Test/QATest_None.py
@@ -8,30 +8,15 @@ from QUANTAXIS.QAUtil import (QADate, QADate_trade );
 
 #from pandas import Series
 import pandas as pd
-import matplotlib as mpl
 
 
 class Test_QA_None(unittest.TestCase):
 
-
-    def fab(self,max):
-        n, a, b = 0, 0, 1
-        while n < max:
-            print (b)
-            a, b = b, a + b
-            n = n + 1
-
-    def test_Generator(self):
-        self.fab(10)
-
     def test_None(self):
 
-        print("----------------------------")
+
         obj = pd.Series([3,4,-2,2])
         print(obj)
-
-
-        pd.Series([1, 23, 4]).plot()
 
         #now = QADate.QA_util_time_now()
         #print( type(now) )
