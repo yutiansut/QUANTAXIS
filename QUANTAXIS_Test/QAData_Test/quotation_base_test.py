12873c7bfdd0ab3ef93e20f5dd46b684ba73a9e0
tauruswang <taurus.wang.xiao.zhen@gmail.com>
2018-05-29 22:09:07 +0800

    Revert "Revert "QARisk 输出 mssage 中输出浮点数 xxx.xx 'annualize_return': round(self.annualize_return,2), 'profit': round(self.profit,2),""
    This reverts commit f60147b

--- QUANTAXIS_Test/QAData_Test/quotation_base_test.py
+++ QUANTAXIS_Test/QAData_Test/quotation_base_test.py
@@ -18,6 +18,8 @@ from QUANTAXIS.QAFetch.QAQuery import (QA_fetch_index_day,
                                        QA_fetch_stock_full,
                                        QA_fetch_stock_min)
 
+from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
+
 import pandas as pd
 from QUANTAXIS.QAData.base_datastruct import _quotation_base
 from QUANTAXIS.QAUtil.QAParameter import FREQUENCE, MARKET_TYPE, DATASOURCE, OUTPUT_FORMAT, DATABASE_TABLE
@@ -38,3 +40,22 @@ class quotation_base_test(unittest.TestCase):
 
 
 
+        # for iRow in range((df_from_Tdx)):
+        #     print(iRow)
+
+    def test_quotation_base_class_itera_(self):
+        qaDAStruct = QA_fetch_stock_day_adv('300439')
+
+        for iRow in qaDAStruct:
+            print(iRow)
+
+        iterObj = qaDAStruct.__iter__()
+
+        i = iterObj.__next__()
+        print(i)
+
+        i = iterObj.__next__()
+        print(i)
+
+        i = iterObj.__next__()
+        print(i)
