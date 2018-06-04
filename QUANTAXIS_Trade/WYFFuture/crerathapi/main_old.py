e01c6a85cb5273a57c1512ab1b411568aa5df97c
wangyufengGH <575063672@qq.com>
2018-06-04 19:44:49 +0800

    #udpate format

--- QUANTAXIS_Trade/WYFFuture/crerathapi/main_old.py
+++ QUANTAXIS_Trade/WYFFuture/crerathapi/main_old.py
@@ -1,10 +1,12 @@
+import ctypes
 import os
 import sys
-import ctypes
-import numpy as np
-import time
 import threading
+import time
 from ctypes import *
+
+import numpy as np
+
 
 def get_timingprice(*instrument_list):
 
@@ -14,12 +16,12 @@ def get_timingprice(*instrument_list):
         closelist.append(price)
 
 
-ip_hq      = 'tcp://180.168.146.187:10011'
-ip_trade   = 'tcp://180.168.146.187:10001'
-broker_id  = "9999"
-account    = input("account")
-pwd        = input("pwd")
-instrument_list = ['j1809','rb1810']
+ip_hq = 'tcp://180.168.146.187:10011'
+ip_trade = 'tcp://180.168.146.187:10001'
+broker_id = "9999"
+account = input("account")
+pwd = input("pwd")
+instrument_list = ['j1809', 'rb1810']
 count = len(instrument_list)
 api = cdll.LoadLibrary('E:\\research\\strategry\\crerathapi\\hqdll.dll')
 
@@ -30,17 +32,17 @@ for items in instrument_list:
 # 转成c++需要的数据格式
 instrument = (c_char_p * len(instrument_list))(*instrument_list_bytes)
 
-api.init(c_char_p(bytes(ip_hq, 'utf-8')),c_char_p(bytes(ip_trade, 'utf-8')),c_char_p(bytes(broker_id, 'utf-8')),c_char_p(bytes(account, 'utf-8')),c_char_p(bytes(pwd, 'utf-8')),instrument,c_int(count))
+api.init(c_char_p(bytes(ip_hq, 'utf-8')), c_char_p(bytes(ip_trade, 'utf-8')), c_char_p(bytes(broker_id,
+                                                                                             'utf-8')), c_char_p(bytes(account, 'utf-8')), c_char_p(bytes(pwd, 'utf-8')), instrument, c_int(count))
 api.creathqapi()
 time.sleep(5)
-api.subscribemarketdata(instrument,2)
+api.subscribemarketdata(instrument, 2)
 time.sleep(5)
 
 # 获取价格
-api.getprice.restype = c_double # 设置python接受dll函数的返回类型
+api.getprice.restype = c_double  # 设置python接受dll函数的返回类型
 price = api.getprice(c_char_p(bytes(instrument_list[0], 'utf-8')))
 print(price)
-
 
 
 # 创建交易实例
@@ -62,21 +64,21 @@ baseprice = 3492
 closelist = []
 
 # 起一个线程获取实时价格
-getprice = threading.Thread( target = get_timingprice, args = (instrument_list[0],))
+getprice = threading.Thread(target=get_timingprice, args=(instrument_list[0],))
 getprice.start()
-p1,p2,p3 = 0,0,0
+p1, p2, p3 = 0, 0, 0
 # orderid = int(round(time.time() * 1000))
 times = 0
 orderid = ''
-api.getdealstatus.restype  = c_double
+api.getdealstatus.restype = c_double
 api.getorderstatus.restype = c_double
 
 while(1):
 
     try:
 
-        price  = closelist[-1]
-        p  = p1
+        price = closelist[-1]
+        p = p1
         p1 = p2
         p2 = p3
         p3 = price
@@ -84,10 +86,10 @@ while(1):
         orderid = str(times+1)
         # 调整的价格必须是期货品种的跳数
         if p3 > p2 and p2 > p1:
-            api.tradeorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(1), c_int(5), c_double(price ),
-                            c_char_p(bytes(orderid, 'utf-8')))
+            api.tradeorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(1), c_int(5), c_double(price),
+                           c_char_p(bytes(orderid, 'utf-8')))
         if p3 < p2 and p2 < p1:
-            api.tradeorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(-1), c_int(5), c_double(price ),
+            api.tradeorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(-1), c_int(5), c_double(price),
                            c_char_p(bytes(orderid, 'utf-8')))
 
         # 撤掉上一笔没有成交的报单
@@ -97,21 +99,20 @@ while(1):
 
         try:
             deal_num = api.getdealstatus(c_char_p(bytes(orderid, 'utf-8')))
-            status   = api.getorderstatus(c_char_p(bytes(orderid, 'utf-8')))
-        except :
+            status = api.getorderstatus(c_char_p(bytes(orderid, 'utf-8')))
+        except:
             pass
 
         if deal_num < 5 and status != 5.0 and status != -1:
             try:
-                api.cancelorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_char_p(bytes(orderid, 'utf-8')))
-            except Exception:
+                api.cancelorder(c_char_p(
+                    bytes(instrument_list[0], 'utf-8')), c_char_p(bytes(orderid, 'utf-8')))
+            except Exception as e:
                 print(e)
-
 
     except Exception as e:
         print(e)
         print("error")
-
 
 
 # orderid = 'abc' # 下单id,以时间戳创建
--- QUANTAXIS_Trade/WYFFuture/crerathapi/trade.py
+++ QUANTAXIS_Trade/WYFFuture/crerathapi/trade.py
@@ -1,18 +1,19 @@
+import ctypes
 import os
 import sys
-import ctypes
-import numpy as np
-import time
 import threading
+import time
 from ctypes import *
 
+import numpy as np
+
 api = cdll.LoadLibrary('E:\\research\\strategry\\crerathapi\\hqdll.dll')
-ip_hq      = 'tcp://180.168.146.187:10011'
-ip_trade   = 'tcp://180.168.146.187:10001'
-broker_id  = "9999"
-account    =input("account")
-pwd        =input("pwd")
-instrument_list = ['T1806','rb1810']
+ip_hq = 'tcp://180.168.146.187:10011'
+ip_trade = 'tcp://180.168.146.187:10001'
+broker_id = "9999"
+account = input("account")
+pwd = input("pwd")
+instrument_list = ['T1806', 'rb1810']
 count = len(instrument_list)
 
 instrument_list_bytes = []
@@ -22,5 +23,5 @@ for items in instrument_list:
 orderid = str(123)
 print(orderid)
 input()
-api.tradeorder( c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(1), c_int(5), c_double(1840),
-                            c_char_p(bytes(orderid, 'utf-8')) )
+api.tradeorder(c_char_p(bytes(instrument_list[0], 'utf-8')), c_int(1), c_int(5), c_double(1840),
+               c_char_p(bytes(orderid, 'utf-8')))
