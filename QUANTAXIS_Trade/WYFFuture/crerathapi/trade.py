4cca509b32cef4c5b490bea7b0e4982a23af0c5f
wangyuefeng <575063672@qq.com>
2018-06-04 19:38:00 +0800

    修改账户密码

--- QUANTAXIS_Trade/WYFFuture/crerathapi/trade.py
+++ QUANTAXIS_Trade/WYFFuture/crerathapi/trade.py
@@ -10,8 +10,8 @@ api = cdll.LoadLibrary('E:\\research\\strategry\\crerathapi\\hqdll.dll')
 ip_hq      = 'tcp://180.168.146.187:10011'
 ip_trade   = 'tcp://180.168.146.187:10001'
 broker_id  = "9999"
-account    = "112310"
-pwd        = "123456"
+account    =input("account")
+pwd        =input("pwd")
 instrument_list = ['T1806','rb1810']
 count = len(instrument_list)
 
