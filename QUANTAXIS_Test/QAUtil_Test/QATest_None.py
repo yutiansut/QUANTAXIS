d5908611994b626408cde92a765c7715fa2b08e2
tauruswang <604829050@qq.com>
2018-06-05 11:48:52 +0800

    测试用例 添加读取 钱龙软件 本地数据的 读取

--- QUANTAXIS_Test/QAUtil_Test/QATest_None.py
+++ QUANTAXIS_Test/QAUtil_Test/QATest_None.py
@@ -1,22 +1,24 @@
-import unittest
-
-import sys
-import time
 import datetime
 import random
+import sys
+import time
+import unittest
 
-#from QUANTAXIS.QAFetch import (QATdx );
-from QUANTAXIS.QAUtil import (QADate, QADate_trade );
-
+import matplotlib as mpl
 #from pandas import Series
 import pandas as pd
-import matplotlib as mpl
 
-#学习 lru_cache
+#from QUANTAXIS.QAFetch import (QATdx );
+from QUANTAXIS.QAUtil import QADate, QADate_trade
+
+# 学习 lru_cache
+
+
 class MyCache:
     '''
 
     '''
+
     def __init__(self):
         '''constructor'''
         self.cache = {}
@@ -39,7 +41,8 @@ class MyCache:
         '''
         if key not in self.cache and len(self.cache) >= self.max_cache_size:
             self.remove_oldest()
-        self.cache[key] = { 'date_accessed': datetime.datetime.now(), 'value': value}
+        self.cache[key] = {
+            'date_accessed': datetime.datetime.now(), 'value': value}
 
     def remove_oldest(self):
         """
@@ -50,10 +53,10 @@ class MyCache:
         for key in self.cache:
             if oldest_entry == None:
                 oldest_entry = key
-                print('assign oldest_entry key',oldest_entry)
+                print('assign oldest_entry key', oldest_entry)
             elif self.cache[key]['date_accessed'] < self.cache[oldest_entry]['date_accessed']:
                 oldest_entry = key
-                print('delete oles key',oldest_entry)
+                print('delete oles key', oldest_entry)
 
         self.cache.pop(oldest_entry)
 
@@ -65,12 +68,11 @@ class MyCache:
         return len(self.cache)
 
 
-
 class Test_QA_None(unittest.TestCase):
 
     def test_LRU(self):
         # 测试缓存
-        keys = ['test', 'red', 'fox', 'fence', 'junk', \
+        keys = ['test', 'red', 'fox', 'fence', 'junk',
                 'other7', 'alpha8', 'bravo9', 'cal10', 'devo11', 'ele12',
                 'other1', 'alpha2', 'bravo3', 'cal4', 'devo5', 'ele6',
                 ]
@@ -85,11 +87,10 @@ class Test_QA_None(unittest.TestCase):
             print("#%s iterations, #%s cached entries" % (i + 1, cache.size))
         pass
 
-
-    def fab(self,max):
+    def fab(self, max):
         n, a, b = 0, 0, 1
         while n < max:
-            print (b)
+            print(b)
             a, b = b, a + b
             n = n + 1
 
@@ -99,9 +100,8 @@ class Test_QA_None(unittest.TestCase):
     def test_None(self):
 
         print("----------------------------")
-        obj = pd.Series([3,4,-2,2])
+        obj = pd.Series([3, 4, -2, 2])
         print(obj)
-
 
         pd.Series([1, 23, 4]).plot()
 
@@ -120,26 +120,25 @@ class Test_QA_None(unittest.TestCase):
         #     sys.stdout.flush()
         # pass
 
-
     def test_make_Series(self):
-        #demo the series
-        obj = pd.Series([1,2,3,4])
+        # demo the series
+        obj = pd.Series([1, 2, 3, 4])
         print(obj.index)
         print(obj.values)
         print("-------------------------------------------")
-        obj2 = pd.Series([1,2,3,4], index=['a','b','c','d'])
+        obj2 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
         print(obj2)
         print(obj2.index)
         print(obj2.values)
         print("-------------------------------------------")
         # another way to think about a Series is as a fixed-length, ordered dict, as it is a mapping of index value to data values.
         # it can be substituted into many functions that expect a dict
-        sdata = { 'Ohio': 3000, 'Texas': 71000, 'Oregon': 1600, 'Utah': 5000}
+        sdata = {'Ohio': 3000, 'Texas': 71000, 'Oregon': 1600, 'Utah': 5000}
         obj4 = pd.Series(sdata)
         print(obj4)
         print("-------------------------------------------")
         state = ['California', 'Ohio', 'Oregon', 'Taxes']
-        obj4 = pd.Series(sdata, index = state)
+        obj4 = pd.Series(sdata, index=state)
         print(obj4)
         print("-------------------------------------------")
         print(pd.isnull(obj4))
@@ -147,9 +146,9 @@ class Test_QA_None(unittest.TestCase):
         print(pd.notnull(obj4))
         print("-------------------------------------------")
 
-        obj_01 = pd.Series([1,2,3,4])
-        obj_o2 = pd.Series([11,22,33,44], index=['aa','bb','cc','dd'])
-        obj_03 = pd.Series([55,66,77,88], index=['bb','cc','dd','ee'])
+        obj_01 = pd.Series([1, 2, 3, 4])
+        obj_o2 = pd.Series([11, 22, 33, 44], index=['aa', 'bb', 'cc', 'dd'])
+        obj_03 = pd.Series([55, 66, 77, 88], index=['bb', 'cc', 'dd', 'ee'])
         print(obj_01)
         print(obj_o2)
         print("-------------------------------------------")
@@ -157,20 +156,20 @@ class Test_QA_None(unittest.TestCase):
         print("-------------------------------------------")
         print(obj_o2 + obj_03)
 
-
     def test_make_dataframe(self):
         # the dataframe has both a row and column index
         dict_data = {
             'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
             'year': [2000, 2001, 2002, 2001, 2002],
-            'pop': [1.4,1.7,3.6,2.4,2.9]
+            'pop': [1.4, 1.7, 3.6, 2.4, 2.9]
         }
 
         frame = pd.DataFrame(dict_data)
         print(frame)
         print("-------------------------------------------")
 
-        frame2 = pd.DataFrame(dict_data, columns=['year','state','pop', 'area'])
+        frame2 = pd.DataFrame(dict_data, columns=[
+                              'year', 'state', 'pop', 'area'])
         print(frame2)
         print("-------------------------------------------")
 
@@ -189,8 +188,7 @@ class Test_QA_None(unittest.TestCase):
         print(frame2.T)
         print("-------------------------------------------")
 
-        pop = { 'Nevada' : {2001: 2.4, 2002: 2.9}, "Ohio": {2000: 1.4,2000: 1.7, 2002: 3.4}}
+        pop = {'Nevada': {2001: 2.4, 2002: 2.9},
+               "Ohio": {2000: 1.4, 2000: 1.7, 2002: 3.4}}
         frame3 = pd.DataFrame(pop)
         print(frame3)
-
-        
