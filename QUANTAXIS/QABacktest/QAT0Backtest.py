c2e2d481e9986e30311a51d58bb63bff4a4ce6b2
yutiansut <yutiansut@qq.com>
2018-06-08 13:14:59 +0800

    #udpate

--- QUANTAXIS/QABacktest/QAT0Backtest.py
+++ QUANTAXIS/QABacktest/QAT0Backtest.py
@@ -7,7 +7,11 @@
 2. 核查每日可用股票,以及是否平仓(及如果尾盘没有买回去,要在结转的时候自动买回/卖出)
 
 """
+from QUANTAXIS.QABacktest.QABacktest import QA_Backtest
 
-class QAT0Backtest():
-    def __init__(self):
-        pass
+
+class QAT0Backtest(QA_Backtest):
+
+    def __init__(self, market_type, frequence, start, end, code_list, commission_fee):
+        super().__init__(market_type, frequence, start, end, code_list, commission_fee)
+        
