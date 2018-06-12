c6f68796888c1369f59e79b5475461c3e6867fa2
tauruswang <604829050@qq.com>
2018-06-13 02:29:39 +0800

    添加获取东方财富资金流向

--- QUANTAXIS/QAUtil/QALocalize.py
+++ QUANTAXIS/QAUtil/QALocalize.py
@@ -6,7 +6,7 @@ qa_path = '{}{}{}'.format(path, os.sep, '.quantaxis')
 setting_path = '{}{}{}'.format(qa_path, os.sep, 'setting')
 cache_path = '{}{}{}'.format(qa_path, os.sep, 'cache')
 log_path = '{}{}{}'.format(qa_path, os.sep, 'log')
-download_path = '{}{}{}'.format(qa_path, os.sep, 'download')
+download_path = '{}{}{}'.format(qa_path, os.sep, 'downloads')
 
 os.makedirs(qa_path, exist_ok=True)
 os.makedirs(setting_path, exist_ok=True)
