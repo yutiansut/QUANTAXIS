d60fc8aa9ad00af6833b9fd8ba65722bf28db928
tauruswang <604829050@qq.com>
2018-06-20 17:14:54 +0800

    获取东方财富资金流向保存到 mongodb中

--- QUANTAXIS/QASU/main.py
+++ QUANTAXIS/QASU/main.py
@@ -226,13 +226,20 @@ def QA_SU_crawl_eastmoney(action="zjlx",stockCode=None):
 
     if stockCode=="all":
         #读取tushare股票列表代码
-        print(" 一共需要获取 %d 个股票的 资金流向 , 需要大概 %d 小时" % (len(stockItems), (len(stockItems)*30)/60/60 ))
+        print("💪 一共需要获取 %d 个股票的 资金流向 , 需要大概 %d 小时" % (len(stockItems), (len(stockItems)*5)/60/60 ))
+
+        code_list = []
         for stock in stockItems:
+            code_list.append(stock['code'])
             #print(stock['code'])
-            crawl_eastmoney_file.QA_read_eastmoney_zjlx_web_page_to_sqllite(stockCode=stock['code'])
+        crawl_eastmoney_file.QA_read_eastmoney_zjlx_web_page_to_sqllite(code_list)
             #print(stock)
 
         return
     else:
         #todo 检查股票代码是否合法
-        return crawl_eastmoney_file.QA_read_eastmoney_zjlx_web_page_to_sqllite(stockCode=stockCode)
+        #return crawl_eastmoney_file.QA_read_eastmoney_zjlx_web_page_to_sqllite(stockCode=stockCode)
+        code_list = []
+        code_list.append(stockCode)
+        return crawl_eastmoney_file.QA_request_eastmoney_zjlx(param_stock_code_list=code_list)
+
