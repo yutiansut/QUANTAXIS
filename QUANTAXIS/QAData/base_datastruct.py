23e0a87a801808400ac1dc2d4e4607bf99b35ec4
yutiansut <yutiansut@qq.com>
2018-07-19 14:55:21 +0800

    #update pivot

--- QUANTAXIS/QAData/base_datastruct.py
+++ QUANTAXIS/QAData/base_datastruct.py
@@ -725,14 +725,14 @@ class _quotation_base():
         """增加对于多列的支持"""
         if isinstance(column_, str):
             try:
-                return self.data.pivot(index='datetime', columns='code', values=column_)
+                return self.data.reset_index().pivot(index='datetime', columns='code', values=column_)
             except:
-                return self.data.pivot(index='date', columns='code', values=column_)
+                return self.data.reset_index().pivot(index='date', columns='code', values=column_)
         elif isinstance(column_, list):
             try:
-                return self.data.pivot_table(index='datetime', columns='code', values=column_)
+                return self.data.reset_index().pivot_table(index='datetime', columns='code', values=column_)
             except:
-                return self.data.pivot_table(index='date', columns='code', values=column_)
+                return self.data.reset_index().pivot_table(index='date', columns='code', values=column_)
 
 
 
