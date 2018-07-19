ffa3c09b82d17aeef270e027eafa5104307acd26
yutiansut <yutiansut@qq.com>
2018-07-19 16:22:23 +0800

    @ 增加实盘易对华福证券/海通证券的支持

--- QUANTAXIS/QAMarket/QAShipaneBroker.py
+++ QUANTAXIS/QAMarket/QAShipaneBroker.py
@@ -284,21 +284,21 @@ class QA_SPEBroker(QA_Broker):
 if __name__ == '__main__':
     a = QA_SPEBroker()
     print('查询账户')
-    print(a.query_accounts('account:1391'))
-    print('查询所有订单')
-    print(a.query_orders('account:1391'))
-    print('查询未成交订单')
-    print(a.query_orders('account:1391', 'open'))
-    """多账户同时下单测试
-    """
-    print('下单测试')
-    print(a.send_order('account:1391', price=9))
-    print('查询新的未成交订单')
-    print(a.query_orders('account:1391', 'open'))
-    print('查询已成交订单')
-    print(a.query_orders('account:1391', 'filled'))
-    # print(a.send_order('account:141',price=8.95))
-    print('一键全部撤单')
-    print(a.cancel_all('account:1391'))
+    print(a.query_positions('account:282'))
+    # print('查询所有订单')
+    # print(a.query_orders('account:1391'))
+    # print('查询未成交订单')
+    # print(a.query_orders('account:1391', 'open'))
+    # """多账户同时下单测试
+    # """
+    # print('下单测试')
+    # print(a.send_order('account:1391', price=9))
+    # print('查询新的未成交订单')
+    # print(a.query_orders('account:1391', 'open'))
+    # print('查询已成交订单')
+    # print(a.query_orders('account:1391', 'filled'))
+    # # print(a.send_order('account:141',price=8.95))
+    # print('一键全部撤单')
+    # print(a.cancel_all('account:1391'))
 
-    print(a.cancel_order('account:141', '1703'))
+    # print(a.cancel_order('account:141', '1703'))
