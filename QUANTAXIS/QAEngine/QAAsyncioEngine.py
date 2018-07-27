c9e94d50a76a7b3597e05a00af6202dca3644ff7
yutiansut <yutiansut@qq.com>
2018-07-28 02:25:05 +0800

    #update

--- QUANTAXIS/QAEngine/QAAsyncioEngine.py
+++ QUANTAXIS/QAEngine/QAAsyncioEngine.py
@@ -1,3 +1,27 @@
 import asyncio
 import future
+import traceback
+import time
+import datetime
+import threading
+
+
+from QUANTAXIS.QAFetch.QAQuery_Async import QA_fetch_stock_day
+
+l=asyncio.get_event_loop()
+z=threading.Thread(target=l.run_forever)
+
+z.start()
+
+
+data_job=asyncio.run_coroutine_threadsafe(QA_fetch_stock_day('000001','2018-01-01','2018-01-31'),l)
+
+
+
+while not data_job.done():
+    print('waiting')
+
+    if data_job.done():
+        print(data_job.result())
+        break
 
