2dc5286f8578097f2a191472bfaf42ae7fa003c8
yutiansut <yutiansut@qq.com>
2018-07-28 02:31:38 +0800

    #update QAAsyncioEngine

--- QUANTAXIS/QAEngine/QAAsyncioEngine.py
+++ QUANTAXIS/QAEngine/QAAsyncioEngine.py
@@ -1,27 +1,29 @@
 import asyncio
-import future
-import traceback
-import time
 import datetime
 import threading
+import time
+import traceback
 
+import future
 
 from QUANTAXIS.QAFetch.QAQuery_Async import QA_fetch_stock_day
 
-l=asyncio.get_event_loop()
-z=threading.Thread(target=l.run_forever)
 
-z.start()
+class QAAsync():
+    def __init__(self):
+        self.event_loop = asyncio.get_event_loop()
+        self.elthread = threading.Thread(target=self.event_loop.run_forever)
+
+        self.elthread.setDaemon(True)
+        self.elthread.start()
+
+    def submit(self,func,*args,**kwargs):
+        self.job=asyncio.run_coroutine_threadsafe(func(*args,**kwargs),self.event_loop)
+        self.callback()
+
+    def callback(self):
+        print(self.job.result(7))
 
 
-data_job=asyncio.run_coroutine_threadsafe(QA_fetch_stock_day('000001','2018-01-01','2018-01-31'),l)
-
-
-
-while not data_job.done():
-    print('waiting')
-
-    if data_job.done():
-        print(data_job.result())
-        break
-
+QAE=QAAsync()
+QAE.submit(QA_fetch_stock_day,'000001','2018-01-01', '2018-01-31')
