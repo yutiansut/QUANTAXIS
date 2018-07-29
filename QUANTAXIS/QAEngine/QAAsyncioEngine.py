36e3e925ed9d0b97c0a150cc81d1086f71af1ed2
yutiansut <yutiansut@qq.com>
2018-07-29 12:29:47 +0800

    #update asyncio

--- QUANTAXIS/QAEngine/QAAsyncioEngine.py
+++ QUANTAXIS/QAEngine/QAAsyncioEngine.py
@@ -14,40 +14,59 @@ class QAAsync():
         self.event_loop = asyncio.get_event_loop()
         self.elthread = threading.Thread(target=self.event_loop.run_forever)
 
-        self.elthread.setDaemon(True)
+        #self.elthread.setDaemon(True)
         self.elthread.start()
 
-    def submit(self,func,*args,**kwargs):
+    def submit(self, func, callback, *args, **kwargs):
 
-        self.job=asyncio.run_coroutine_threadsafe(func(*args,**kwargs),self.event_loop)
-        self.callback()
+        # self.job=asyncio.run_coroutine_threadsafe(func(*args,**kwargs),self.event_loop)
 
-    def callback(self):
-        r=self.job.result(0.05)
-        print(len(r))
+        task = asyncio.ensure_future(func(*args, **kwargs))
+        task.add_done_callback(callback)
+        #print('get job async {}'.format(args))
 
 
-QAE=QAAsync()
+def callback(future):
+    r = future.result()
+    print(len(r))
+    print(datetime.datetime.now()-time)
 
-time=datetime.datetime.now()
-QAE.submit(QA_fetch_stock_day,'000001','2018-01-01', '2018-01-31')
-QAE.submit(QA_fetch_stock_day,'000003','2018-01-01', '2018-01-31')
-QAE.submit(QA_fetch_stock_day,'000007','2018-01-01', '2018-01-31')
-QAE.submit(QA_fetch_stock_day,'000004','2018-01-01', '2018-01-31')
-QAE.submit(QA_fetch_stock_day,'000005','2018-01-01', '2018-01-31')
-print(datetime.datetime.now()-time)
 
-import QUANTAXIS as QA
-time=datetime.datetime.now()
-r=QA.QA_fetch_stock_day('000008','2018-01-01', '2018-01-31')
-print(len(r))
-print(datetime.datetime.now()-time)
-r=QA.QA_fetch_stock_day('000009','2018-01-01', '2018-01-31')
-print(len(r))
-r=QA.QA_fetch_stock_day('600000','2018-01-01', '2018-01-31')
-print(len(r))
-r=QA.QA_fetch_stock_day('600014','2018-01-01', '2018-01-31')
-print(len(r))
-r=QA.QA_fetch_stock_day('600010','2018-01-01', '2018-01-31')
-print(len(r))
-print(datetime.datetime.now()-time)
+"""
+
+run_until_complete 
+
+"""
+
+
+if __name__ == '__main__':
+    time = datetime.datetime.now()
+    QAE = QAAsync()
+
+    
+    print(datetime.datetime.now()-time)
+    QAE.submit(QA_fetch_stock_day, callback,
+               '000001', '1990-01-01', '2018-01-31')
+    QAE.submit(QA_fetch_stock_day, callback,
+               '000002', '1990-01-01', '2018-01-31')
+    QAE.submit(QA_fetch_stock_day, callback,
+               '000007', '1990-01-01', '2018-01-31')
+    QAE.submit(QA_fetch_stock_day, callback,
+               '000004', '1990-01-01', '2018-01-31')
+    QAE.submit(QA_fetch_stock_day, callback,
+               '000005', '1990-01-01', '2018-01-31')
+
+    # import QUANTAXIS as QA
+    # time=datetime.datetime.now()
+    # r=QA.QA_fetch_stock_day('000001','1990-01-01', '2018-01-31')
+    # print(len(r))
+    # #print(datetime.datetime.now()-time)
+    # r=QA.QA_fetch_stock_day('000002','1990-01-01', '2018-01-31')
+    # print(len(r))
+    # r=QA.QA_fetch_stock_day('000007','1990-01-01', '2018-01-31')
+    # print(len(r))
+    # r=QA.QA_fetch_stock_day('000004','1990-01-01', '2018-01-31')
+    # print(len(r))
+    # r=QA.QA_fetch_stock_day('000005','1990-01-01', '2018-01-31')
+    # print(len(r))
+    # print(datetime.datetime.now()-time)
