5e49fba961a8a7405027857c9e903d023405c84b
tauruswang <604829050@qq.com>
2018-07-31 11:30:16 +0800

    GUI 数据获取 多给进程获取 eastmoney 资金流向的实现

--- QUANTAXIS_Monitor_GUI/MainWin.py
+++ QUANTAXIS_Monitor_GUI/MainWin.py
@@ -11,23 +11,28 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from QUANTAXIS_Monitor_GUI.MainWindow.TabDataMaintenance import *
-from QUANTAXIS_Monitor_GUI.MainWindow.TabWebpageCrawly import *
+
+from QUANTAXIS_Monitor_GUI.MainTabWindows.Tab00_WelcomeSplash import *
+
+
+from QUANTAXIS_Monitor_GUI.MainTabWindows.Tab01_DataMaintenance import *
+from QUANTAXIS_Monitor_GUI.MainTabWindows.Tab02_WebpageCrawly import *
 #from QUANTAXIS_Monitor_GUI.MainWindow.TabForecastStockTrends import *
 
 class TabDemo(QTabWidget):
     def __init__(self, parent=None):
         super(TabDemo, self).__init__(parent)
 
-        self.tab1 = TabDataMaintenance()
-        self.tab2 = TabWebpageCrawly()
+        self.tab0 = TabWelcomeSplash(parent=self)
+        self.tab1 = TabDataMaintenance(parent=self)
+        self.tab2 = TabWebpageCrawly(parent=self)
         self.tab3 = QWidget()
         self.tab4 = QWidget()
         self.tab5 = QWidget()
         #self.tab6 = TabForecastStockTrends()
         self.tab7 = QWidget()
 
-
+        self.addTab(self.tab0, "欢迎")
         self.addTab(self.tab1, "数据下载")
         self.addTab(self.tab2, "网页数据抓取")
         self.addTab(self.tab3, "数据比对清洗")
@@ -36,21 +41,25 @@ class TabDemo(QTabWidget):
         #self.addTab(self.tab6, "近期上涨下跌股票预测")
         self.addTab(self.tab7, "系统配置信息")
 
-
+        self.tab0.initUI()
         self.tab1.initUI()
-        self.setTabText(0, "    🗂   数据维护                                        ")
-        self.setTabText(1, "    📑   网页数据抓取                                      ")
-        self.setTabText(2, "    🖇   数据比对清洗                                      ")
-        self.setTabText(3, "    🔍   数据盘后分析任务                                  ")
-        self.setTabText(4, "    🎲   策略概率回测                                    ")
-        self.setTabText(5, "    📈   近期上涨下跌股票预测                              ")
-        self.setTabText(6, "    🛠   系统配置信息                                    ")
+        self.tab2.initUI()
+
+
+        self.setTabText(0, "    🎉   欢迎                                        ")
+        self.setTabText(1, "    🗂   数据维护                                        ")
+        self.setTabText(2, "    📑   网页数据抓取                                      ")
+        self.setTabText(3, "    🖇   数据比对清洗                                      ")
+        self.setTabText(4, "    🔍   数据盘后分析任务                                  ")
+        self.setTabText(5, "    🎲   策略概率回测                                    ")
+        self.setTabText(6, "    📈   近期上涨下跌股票预测                              ")
+        self.setTabText(7, "    🛠   系统配置信息                                    ")
 
 
         #self.tab2UI()
         #self.tab3UI()
         #self.tab4UI()
-
+        self.setMinimumHeight(700)
         self.setWindowTitle("QUANTAXIS MONITOR ver.0.0.0.1")
         #self.setMinimumHeight(800)
         #self.setMinimumWidth(1000)
