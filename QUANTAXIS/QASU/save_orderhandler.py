1699a8c936e8a4da809929f4fa735fd1f8bca680
yutiansut <yutiansut@qq.com>
2018-08-08 20:26:13 +0800

    #update

--- QUANTAXIS/QASU/save_orderhandler.py
+++ QUANTAXIS/QASU/save_orderhandler.py
@@ -1,7 +1,50 @@
-# 
+# coding:utf-8
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2018 yutiansut/QUANTAXIS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-from QUANTAXIS.QAUtil import DATABASE
+
+from QUANTAXIS.QAUtil import DATABASE, QA_util_date_int2str
+from QUANTAXIS.QAUtil.QASql import DESCENDING, ASCENDING
 
 
+def QA_SU_save_order(orderlist, client=DATABASE):
+    collection = client.order
+    collection.create_index(
+        [('account_cookie', ASCENDING), ('realorder_id', ASCENDING)], unique=True)
+    try:
+        for item in orderlist:
+            collection.update({'account_cookie': item.pop('account_cookie'), 'realorder_id': item.pop('realorder_id')},
+                              {'$inc': {'$set': {item}}}, upsert=True)
+    except Exception as e:
+
+        pass
 
 
+def QA_SU_save_deal(dealist, client=DATABASE):
+    collection = client.deal
+    collection.create_index(
+        [('account_cookie', ASCENDING), ('realorder_id', ASCENDING)], unique=True)
+    try:
+        collection.insert_many(dealist)
+    except Exception as e:
+        pass
