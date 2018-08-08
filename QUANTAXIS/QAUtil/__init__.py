7d7977d3c478b8968983407aba917be88435f107
yutiansut <yutiansut@qq.com>
2018-08-09 01:06:47 +0800

    #update

--- QUANTAXIS/QAUtil/__init__.py
+++ QUANTAXIS/QAUtil/__init__.py
@@ -54,6 +54,7 @@ from QUANTAXIS.QAUtil.QADate_trade import (QA_util_date_gap,
                                            QA_util_if_tradetime,
                                            QA_util_get_next_day,
                                            QA_util_get_last_day,
+                                           QA_util_get_order_day,
                                            trade_date_sse)
 # list function
 from QUANTAXIS.QAUtil.QAList import (QA_util_diff_list,
--- QUANTAXIS/__init__.py
+++ QUANTAXIS/__init__.py
@@ -135,7 +135,7 @@ from QUANTAXIS.QASetting.QALocalize import qa_path, setting_path, cache_path, do
 
 
 from QUANTAXIS.QAUtil import (QA_util_date_stamp, QA_util_time_stamp, QA_util_ms_stamp, QA_util_date_valid, QA_util_calc_time,
-                              QA_util_realtime, QA_util_id2date, QA_util_is_trade, QA_util_get_date_index, QA_util_get_last_day, QA_util_get_next_day,
+                              QA_util_realtime, QA_util_id2date, QA_util_is_trade, QA_util_get_date_index, QA_util_get_last_day, QA_util_get_next_day, QA_util_get_order_day,
                               QA_util_get_index_date, QA_util_select_hours, QA_util_date_gap, QA_util_time_gap,
                               QA_util_select_min, QA_util_time_delay, QA_util_time_now, QA_util_date_str2int,
                               QA_util_date_int2str, QA_util_date_today, QA_util_to_datetime,
