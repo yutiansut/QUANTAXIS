2e19198881d2e7fa2352dad8bb13bc7d8263321a
yutiansut <yutiansut@qq.com>
2018-08-11 15:16:02 +0800

    #

--- QUANTAXIS/QAMarket/QABacktestBroker.py
+++ QUANTAXIS/QAMarket/QABacktestBroker.py
@@ -213,6 +213,8 @@ class QA_BacktestBroker(QA_Broker):
             return pd.DataFrame(list(self.deal_message.values()), columns=self.orderstatus_headers).set_index(['account_cookie', 'realorder_id'])
         elif status == 'filled':
             pass
+        elif status == 'open':
+            pass
 
     def query_deal(self,account):
         pass
