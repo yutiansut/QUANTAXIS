45b87c679cac3a7c127f750b5f6c2e35d6bcc37c
yutiansut <yutiansut@qq.com>
2018-09-13 20:35:12 +0800

    #update for save strategy

--- QUANTAXIS/QASU/save_strategy.py
+++ QUANTAXIS/QASU/save_strategy.py
@@ -23,9 +23,29 @@
 # SOFTWARE.
 
 from QUANTAXIS.QAUtil import DATABASE
-
+from QUANTAXIS.QASetting.QALocalize import strategy_path
+import datetime
+import os
+import sys
 """对于策略的存储
 """
 
-def QA_SU_save_strategy():
-    pass
+
+def QA_SU_save_strategy(name, topic='default', version=1, if_save=False):
+    absoult_path = '{}{}strategy_{}.py'.format(strategy_path, os.sep, name)
+    with open(sys.argv[0], 'rb') as p:
+        data = p.read()
+        collection = DATABASE.strategy
+        collection.insert({'name': name,
+                           'topic': topic, 'version': version,
+                           'last_modify_time': datetime.datetime.now(),
+                           'content': data.decode('utf-8'),
+                           'absoultpath': absoult_path})
+        if if_save:
+            with open(absoult_path, 'wb') as f:
+                f.write(data)
+
+
+# print(os.path.basename(sys.argv[0]))
+if __name__ == '__main__':
+    QA_SU_save_strategy('test', if_save=True)
