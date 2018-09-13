a6a123bf35e1f6128f9417f24ce99f57c37b7c8a
yutiansut <yutiansut@qq.com>
2018-09-13 11:27:14 +0800

    #update the requirements

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
     ],
-    install_requires=['pandas>=0.22.0', 'numpy>=1.12.0', 'tushare', 'flask_socketio>=2.9.0 ', 'motor>=1.1', 'seaborn>=0.8.1',
+    install_requires=['pandas>=0.23.4', 'numpy>=1.12.0', 'tushare', 'flask_socketio>=2.9.0 ', 'motor>=1.1', 'seaborn>=0.8.1',
                       'lxml>=4.0', ' beautifulsoup4', 'flask-socketio', 'flask', 'matplotlib', 'requests', 'selenium', 'tornado',
                       'pymongo>=3.7', 'six>=1.10.0', 'tabulate>=0.7.7', 'pytdx>=1.67', 'retrying>=1.3.3', 'scrapy', 'pyecharts-jupyter-installer',
                       'zenlog>=1.1', 'delegator.py>=0.0.12', 'flask>=0.12.2', 'pyecharts==0.4.1', 'protobuf>=3.4.0'],
