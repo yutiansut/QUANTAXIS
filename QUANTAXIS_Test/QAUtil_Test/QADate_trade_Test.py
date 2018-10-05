649c3246818dc194183f66d01b40fb3181b40b94
yutiansut <yutiansut@qq.com>
2018-10-06 00:20:19 +0800

    #update

--- QUANTAXIS_Test/QAUtil_Test/QADate_trade_Test.py
+++ QUANTAXIS_Test/QAUtil_Test/QADate_trade_Test.py
@@ -8,16 +8,15 @@ from QUANTAXIS.QAUtil import QADate, QADate_trade
 class Test_QA_Date_trade(unittest.TestCase):
     def test_QA_util_if_trade(self):
 
-        now = QADate.QA_util_time_now()
-        str_from_today = '%04d-%02d-%02d' % (now.year, now.month, now.day)
-
+        #str_from_today = '%04d-%02d-%02d' % (now.year, now.month, now.day)
+        str_from_today=str(datetime.date.today())
         nDayLeft = 1000
         while nDayLeft > 0:
 
             toDayIsTradeDay = QADate_trade.QA_util_if_trade(str_from_today)
             realTradeDay = QADate_trade.QA_util_get_real_date(str_from_today)
 
-            QADate.QA_util_date_valid(realTradeDay)
+            QADate.QA_util_date_valid(realTradeDWWWay)
 
             if toDayIsTradeDay == False:
                 prev_trade_day = QADate_trade.QA_util_get_last_day(
