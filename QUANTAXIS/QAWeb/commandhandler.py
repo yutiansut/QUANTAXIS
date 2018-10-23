a1ac11e2c0a32be0187b0359ae13d7a1e7c2d428
yutiansut <yutiansut@qq.com>
2018-10-23 17:33:13 +0800

    #

--- QUANTAXIS/QAWeb/commandhandler.py
+++ QUANTAXIS/QAWeb/commandhandler.py
@@ -9,6 +9,7 @@ from tornado.web import Application, RequestHandler, authenticated
 from tornado.websocket import WebSocketHandler
 
 from QUANTAXIS.QAWeb.basehandles import QABaseHandler, QAWebSocketHandler
+from QUANTAXIS.QAUtil.QADict import QA_util_dict_remove_key
 
 
 class CommandHandler(QABaseHandler):
@@ -48,6 +49,46 @@ class RunnerHandler(QAWebSocketHandler):
     def on_close(self):
         pass
         # self.write_message('close')
+
+
+class JOBHandler(QABaseHandler):
+    """job handler
+
+    Arguments:
+        QABaseHandler {[type]} -- [description]
+    """
+
+    def post(self):
+        try:
+            from quantaxis_run import quantaxis_run
+        except:
+            self.write('no quantaxis_run program on this server')
+            return
+
+        program = self.get_argument('program', 'python')
+        files = self.get_argument('jobfile', False)
+        if files:
+            #self.wirte({'QUANTAXIS RUN': files})
+            res = quantaxis_run.delay(files, program)
+            self.write({'status': 'pending', 'job_id': str(res.id)})
+        else:
+            self.write({'status': 'error'})
+
+            #shell_cmd = 'python "{}"'.format(shell_cmd)
+
+    def get(self):
+        try:
+            from quantaxis_run.query import query_result, query_onejob
+        except:
+            self.write('no quantaxis_run program on this server')
+            return
+        job_id = self.get_argument('job_id', 'all')
+        if job_id == 'all':
+            self.write({'result': [QA_util_dict_remove_key(
+                item, '_id') for item in query_result()]})
+        else:
+            self.wirte({'result': [QA_util_dict_remove_key(
+                item, '_id') for item in query_onejob(job_id)]})
 
 
 if __name__ == "__main__":
