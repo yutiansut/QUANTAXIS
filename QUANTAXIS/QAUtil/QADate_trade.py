654fe3ae8d522b7bf11cbf4f92342c289b643206
yutiansut <yutiansut@qq.com>
2019-01-07 05:22:22 +0800

    #

--- QUANTAXIS/QAUtil/QADate_trade.py
+++ QUANTAXIS/QAUtil/QADate_trade.py
@@ -473,7 +473,7 @@ def QA_util_future_to_tradedatetime(real_datetime):
     Returns:
         [type] -- [description]
     """
-    if len(str(real_datetime))==19:
+    if len(str(real_datetime))>=19:
         dt = datetime.datetime.strptime(str(real_datetime)[0:19], '%Y-%m-%d %H:%M:%S')
         return dt if dt.time()<datetime.time(21,0) else QA_util_get_next_datetime(dt,1)
     elif len(str(real_datetime))==16:
