f858f6c6ec55aea9d0a2828844029b99e56a7dcd
yutiansut <yutiansut@qq.com>
2019-01-12 11:43:57 +0800

    #update yapf

--- QUANTAXIS/QAEngine/QATask.py
+++ QUANTAXIS/QAEngine/QATask.py
@@ -21,14 +21,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 from QUANTAXIS.QAUtil.QARandom import QA_util_random_with_topic
-
-
 """标准的QUANTAXIS事件方法,具有QA_Thread,QA_Event等特性,以及一些日志和外部接口"""
 
 
 class QA_Task():
+
     def __init__(self, worker, event, engine=None, callback=False):
         self.worker = worker
         self.event = event
@@ -38,7 +36,12 @@ class QA_Task():
         self.engine = engine
 
     def __repr__(self):
-        return '< QA_Task engine {} , worker {} , event {},  id = {} >'.format(self.engine,self.worker, self.event, id(self))
+        return '< QA_Task engine {} , worker {} , event {},  id = {} >'.format(
+            self.engine,
+            self.worker,
+            self.event,
+            id(self)
+        )
 
     def do(self):
         self.res = self.worker.run(self.event)
@@ -53,10 +56,7 @@ class QA_Task():
         #     'worker': self.worker,
         #     'event': self.event
         # }
-        return {
-            'task_id': self.task_id,
-            'result': self.res
-        }
+        return {'task_id': self.task_id, 'result': self.res}
 
 
 if __name__ == '__main__':
