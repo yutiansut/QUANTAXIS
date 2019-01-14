6e44fb9c162e90b117d588123d7105bf5c574b42
yutiansut <yutiansut@qq.com>
2019-01-14 17:12:24 +0800

    #

--- QUANTAXIS/QAUtil/QABar.py
+++ QUANTAXIS/QAUtil/QABar.py
@@ -28,10 +28,13 @@ import math
 import numpy as np
 import pandas as pd
 
-from QUANTAXIS.QAUtil.QADate_trade import (QA_util_date_gap,
-                                           QA_util_get_real_datelist,
-                                           QA_util_get_trade_range,
-                                           QA_util_if_trade, trade_date_sse)
+from QUANTAXIS.QAUtil.QADate_trade import (
+    QA_util_date_gap,
+    QA_util_get_real_datelist,
+    QA_util_get_trade_range,
+    QA_util_if_trade,
+    trade_date_sse
+)
 
 
 def QA_util_make_futuremin_index(day, type_='1min'):
@@ -45,8 +48,19 @@ def QA_util_make_futuremin_index(day, type_='1min'):
     """
 
     if QA_util_if_trade(day) is True:
-        return pd.date_range(str(day) + '21:00:00', str(day) + '', freq=type_, closed='right').append(
-            pd.date_range(str(day) + ' 13:00:00', str(day) + ' 15:00:00', freq=type_, closed='right'))
+        return pd.date_range(
+            str(day) + '21:00:00',
+            str(day) + '',
+            freq=type_,
+            closed='right'
+        ).append(
+            pd.date_range(
+                str(day) + ' 13:00:00',
+                str(day) + ' 15:00:00',
+                freq=type_,
+                closed='right'
+            )
+        )
     else:
         return pd.DataFrame(['No trade'])
 
@@ -62,8 +76,19 @@ def QA_util_make_min_index(day, type_='1min'):
     """
 
     if QA_util_if_trade(day) is True:
-        return pd.date_range(str(day) + ' 09:30:00', str(day) + ' 11:30:00', freq=type_, closed='right').append(
-            pd.date_range(str(day) + ' 13:00:00', str(day) + ' 15:00:00', freq=type_, closed='right'))
+        return pd.date_range(
+            str(day) + ' 09:30:00',
+            str(day) + ' 11:30:00',
+            freq=type_,
+            closed='right'
+        ).append(
+            pd.date_range(
+                str(day) + ' 13:00:00',
+                str(day) + ' 15:00:00',
+                freq=type_,
+                closed='right'
+            )
+        )
     else:
         return pd.DataFrame(['No trade'])
 
@@ -79,8 +104,19 @@ def QA_util_make_hour_index(day, type_='1h'):
     """
 
     if QA_util_if_trade(day) is True:
-        return pd.date_range(str(day) + ' 09:30:00', str(day) + ' 11:30:00', freq=type_, closed='right').append(
-            pd.date_range(str(day) + ' 13:00:00', str(day) + ' 15:00:00', freq=type_, closed='right'))
+        return pd.date_range(
+            str(day) + ' 09:30:00',
+            str(day) + ' 11:30:00',
+            freq=type_,
+            closed='right'
+        ).append(
+            pd.date_range(
+                str(day) + ' 13:00:00',
+                str(day) + ' 15:00:00',
+                freq=type_,
+                closed='right'
+            )
+        )
     else:
         return pd.DataFrame(['No trade'])
 
@@ -91,24 +127,92 @@ def QA_util_time_gap(time, gap, methods, type_):
     day_gap = math.ceil(gap / min_len)
 
     if methods in ['>', 'gt']:
-        data = pd.concat([pd.DataFrame(QA_util_make_min_index(day, type_)) for day in trade_date_sse[trade_date_sse.index(str(datetime.datetime.strptime(
-            time, '%Y-%m-%d %H:%M:%S').date())):trade_date_sse.index(str(datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').date())) + day_gap + 1]]).reset_index()
-        return np.asarray(data[data[0] > time].head(gap)[0].apply(lambda x: str(x))).tolist()[-1]
+        data = pd.concat(
+            [
+                pd.DataFrame(QA_util_make_min_index(day,
+                                                    type_))
+                for day in trade_date_sse[trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ):trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) + day_gap + 1]
+            ]
+        ).reset_index()
+        return np.asarray(
+            data[data[0] > time].head(gap)[0].apply(lambda x: str(x))
+        ).tolist()[-1]
     elif methods in ['>=', 'gte']:
-        data = pd.concat([pd.DataFrame(QA_util_make_min_index(day, type_)) for day in trade_date_sse[trade_date_sse.index(str(datetime.datetime.strptime(
-            time, '%Y-%m-%d %H:%M:%S').date())):trade_date_sse.index(str(datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').date())) + day_gap + 1]]).reset_index()
+        data = pd.concat(
+            [
+                pd.DataFrame(QA_util_make_min_index(day,
+                                                    type_))
+                for day in trade_date_sse[trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ):trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) + day_gap + 1]
+            ]
+        ).reset_index()
 
-        return np.asarray(data[data[0] >= time].head(gap)[0].apply(lambda x: str(x))).tolist()[-1]
+        return np.asarray(
+            data[data[0] >= time].head(gap)[0].apply(lambda x: str(x))
+        ).tolist()[-1]
     elif methods in ['<', 'lt']:
-        data = pd.concat([pd.DataFrame(QA_util_make_min_index(day, type_)) for day in trade_date_sse[trade_date_sse.index(str(datetime.datetime.strptime(
-            time, '%Y-%m-%d %H:%M:%S').date())) - day_gap:trade_date_sse.index(str(datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').date())) + 1]]).reset_index()
+        data = pd.concat(
+            [
+                pd.DataFrame(QA_util_make_min_index(day,
+                                                    type_))
+                for day in trade_date_sse[trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) - day_gap:trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) + 1]
+            ]
+        ).reset_index()
 
-        return np.asarray(data[data[0] < time].tail(gap)[0].apply(lambda x: str(x))).tolist()[0]
+        return np.asarray(
+            data[data[0] < time].tail(gap)[0].apply(lambda x: str(x))
+        ).tolist()[0]
     elif methods in ['<=', 'lte']:
-        data = pd.concat([pd.DataFrame(QA_util_make_min_index(day, type_)) for day in trade_date_sse[trade_date_sse.index(str(datetime.datetime.strptime(
-            time, '%Y-%m-%d %H:%M:%S').date())) - day_gap:trade_date_sse.index(str(datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').date())) + 1]]).reset_index()
+        data = pd.concat(
+            [
+                pd.DataFrame(QA_util_make_min_index(day,
+                                                    type_))
+                for day in trade_date_sse[trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) - day_gap:trade_date_sse.index(
+                    str(
+                        datetime.datetime.strptime(time,
+                                                   '%Y-%m-%d %H:%M:%S').date()
+                    )
+                ) + 1]
+            ]
+        ).reset_index()
 
-        return np.asarray(data[data[0] <= time].tail(gap)[0].apply(lambda x: str(x))).tolist()[0]
+        return np.asarray(
+            data[data[0] <= time].tail(gap)[0].apply(lambda x: str(x))
+        ).tolist()[0]
     elif methods in ['==', '=', 'eq']:
         return time
 
@@ -163,7 +267,6 @@ def QA_util_time_gap(time, gap, methods, type_):
 
 
 """
-
 
 if __name__ == '__main__':
     pass
