c95c8bc9418b4ae9376d396b77bb7405afb0e4cf
yutiansut <yutiansut@qq.com>
2019-03-04 04:12:49 +0800

    #

--- QUANTAXIS/QAARP/QARisk.py
+++ QUANTAXIS/QAARP/QARisk.py
@@ -178,7 +178,7 @@ class QA_Risk():
                 if self.account.market_type == MARKET_TYPE.FUTURE_CN and self.account.allow_margin == True:
                     print('margin!')
                     self._assets = (
-                        self.market_value.sum(axis=1) + self.account.daily_frozen.fillna(0) +
+                        self.market_value.sum(axis=1) + self.account.daily_frozen.fillna(method='pad') +
                         self.account.daily_cash.set_index('date').cash
                     ).fillna(method='pad')
                 else:
