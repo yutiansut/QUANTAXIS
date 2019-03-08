3087706a1551c7a84572f1666ab42958c26cd4f5
zhongjy <zhongjy1992@outlook.com>
2019-03-08 17:33:48 +0800

    format: yapf -i --style qa_style.yapf backtest.py

--- EXAMPLE/4_回测实盘交易/回测/股票回测/基于QAMARKET的回测/backtest.py
+++ EXAMPLE/4_回测实盘交易/回测/股票回测/基于QAMARKET的回测/backtest.py
@@ -22,7 +22,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 from QUANTAXIS.QAARP.QARisk import QA_Risk
 from QUANTAXIS.QAARP.QAUser import QA_User
 from QUANTAXIS.QAApplication.QABacktest import QA_Backtest
@@ -38,23 +37,45 @@ class Backtest(QA_Backtest):
 
     '''
 
-    def __init__(self, market_type, frequence, start, end, code_list, commission_fee):
-        super().__init__(market_type,  frequence, start, end, code_list, commission_fee)
-        mastrategy = MAStrategy(user_cookie=self.user.user_cookie, portfolio_cookie= self.portfolio.portfolio_cookie, account_cookie= 'mastrategy')
+    def __init__(
+            self,
+            market_type,
+            frequence,
+            start,
+            end,
+            code_list,
+            commission_fee
+    ):
+        super().__init__(
+            market_type,
+            frequence,
+            start,
+            end,
+            code_list,
+            commission_fee
+        )
+        mastrategy = MAStrategy(
+            user_cookie=self.user.user_cookie,
+            portfolio_cookie=self.portfolio.portfolio_cookie,
+            account_cookie='mastrategy'
+        )
         #maminstrategy = MAMINStrategy()
         self.account = self.portfolio.add_account(mastrategy)
 
     def after_success(self):
         QA_util_log_info(self.account.history_table)
-        risk = QA_Risk(self.account, benchmark_code='000300',
-                       benchmark_type=MARKET_TYPE.INDEX_CN)
+        risk = QA_Risk(
+            self.account,
+            benchmark_code='000300',
+            benchmark_type=MARKET_TYPE.INDEX_CN
+        )
 
         print(risk().T)
-        fig=risk.plot_assets_curve()
+        fig = risk.plot_assets_curve()
         fig.show()
-        fig=risk.plot_dailyhold()
+        fig = risk.plot_dailyhold()
         fig.show()
-        fig=risk.plot_signal()
+        fig = risk.plot_signal()
         fig.show()
         self.user.save()
         risk.save()
@@ -62,12 +83,14 @@ class Backtest(QA_Backtest):
 
 def run_daybacktest():
     import QUANTAXIS as QA
-    backtest = Backtest(market_type=MARKET_TYPE.STOCK_CN,
-                        frequence=FREQUENCE.DAY,
-                        start='2017-01-01',
-                        end='2017-02-10',
-                        code_list=QA.QA_fetch_stock_block_adv().code[0:5],
-                        commission_fee=0.00015)
+    backtest = Backtest(
+        market_type=MARKET_TYPE.STOCK_CN,
+        frequence=FREQUENCE.DAY,
+        start='2017-01-01',
+        end='2017-02-10',
+        code_list=QA.QA_fetch_stock_block_adv().code[0:5],
+        commission_fee=0.00015
+    )
     # fixed: backtest内部没有自动创建account, 需要手动创建
     backtest._generate_account()
     print(backtest.account)
@@ -79,12 +102,14 @@ def run_daybacktest():
 
 def run_minbacktest():
     import QUANTAXIS as QA
-    backtest = Backtest(market_type=MARKET_TYPE.STOCK_CN,
-                        frequence=FREQUENCE.FIFTEEN_MIN,
-                        start='2017-11-01',
-                        end='2017-11-10',
-                        code_list=QA.QA_fetch_stock_block_adv().code[0:5],
-                        commission_fee=0.00015)
+    backtest = Backtest(
+        market_type=MARKET_TYPE.STOCK_CN,
+        frequence=FREQUENCE.FIFTEEN_MIN,
+        start='2017-11-01',
+        end='2017-11-10',
+        code_list=QA.QA_fetch_stock_block_adv().code[0:5],
+        commission_fee=0.00015
+    )
     backtest.start_market()
 
     backtest.run()
@@ -94,4 +119,3 @@ def run_minbacktest():
 if __name__ == '__main__':
     run_daybacktest()
     #run_minbacktest()
-
