62a868e6a2e5fd9bfd1a391426bfd64ec6956d64
Zeroun <Zeroun@github.com>
2019-04-03 11:24:45 +0800

    修正port必须为int型的bug

--- QUANTAXIS/QAMarket/QATTSBroker.py
+++ QUANTAXIS/QAMarket/QATTSBroker.py
@@ -95,7 +95,7 @@ class QA_TTSBroker(QA_Broker):
                (MARKET_TYPE.FUND_CN, FREQUENCE.THIRTY_MIN): QA_fetch_get_index_min,
                (MARKET_TYPE.FUND_CN, FREQUENCE.SIXTY_MIN): QA_fetch_get_index_min}
 
-    def __init__(self):
+    def __init__(self, auto_logon=True):
         super().__init__()
         self.name = BROKER_TYPE.TTS
         self.config = TTSConfig()
@@ -124,7 +124,8 @@ class QA_TTSBroker(QA_Broker):
         self.gddm_sh = 0  # 上海股东代码
         self.gddm_sz = 0  # 深圳股东代码
 
-        self.logon()
+        if auto_logon is True:
+            self.logon()
 
     def call(self, func, params=None):
 
@@ -223,7 +224,7 @@ class QA_TTSBroker(QA_Broker):
     def logon(self):
         data = self.call("logon", {
             "ip": self.config.values['tdx_server_ip'],
-            "port": self.config.values['tdx_server_port'],
+            "port": int(self.config.values['tdx_server_port']),
             "version": self.config.values['tdx_version'],
             "yyb_id": int(self.config.values['user_yyb']),
             "account_no": self.config.values['user_name'],
@@ -416,20 +417,14 @@ if __name__ == "__main__":
     import os
     import QUANTAXIS as QA
     print('在运行前 请先运行tdxtradeserver的 exe文件, 目录是你直接get_tts指定的 一般是 C:\tdxTradeServer')
-    print('这是测试代码, 下面需要输入的 key/iv在ini中自己查找, account 和password是自己的账户密码 ')
-    api = QA_TTSBroker(endpoint="http://127.0.0.1:19820/api",
-                       enc_key=bytes(input('env_key:   '), encoding='utf-8'), enc_iv=bytes(input('env_iv:    '), encoding='utf-8'))
+    api = QA_TTSBroker(auto_logon=False)
 
     print("---Ping---")
     result = api.ping()
     print(result)
 
     print("---登入---")
-    acc = input('account:    ')
-    password = input('password:   ')
-    result = api.logon("60.191.116.36", 7708,
-                       "6.44", 1,
-                       acc, acc, password, "")
+    result = api.logon()
 
     if result["success"]:
         for i in (0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15):
@@ -442,6 +437,6 @@ if __name__ == "__main__":
         if str(input('我已知晓, 并下单 按y继续 n 退出'))[0] == 'y':
 
             print(api.send_order(code='000001', price=9.8, amount=100,
-                                 towards=QA.ORDER_DIRECTION.BUY, order_model=QA.ORDER_DIRECTION.BUY))
+                                 towards=QA.ORDER_DIRECTION.BUY, order_model=QA.ORDER_MODEL.LIMIT))
         print("---登出---")
         print(api.logoff())
