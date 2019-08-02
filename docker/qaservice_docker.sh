03446872b4a3bbe098372223b380cf8c494f4f91
Vincent yu <yutiansut@qq.com>
2019-08-02 19:07:25 +0800

    Update qaservice_docker.sh

--- docker/qaservice_docker.sh
+++ docker/qaservice_docker.sh
@@ -4,4 +4,5 @@ mkdir quantaxis_docker && cd quantaxis_docker
 wget https://github.com/QUANTAXIS/QUANTAXIS/blob/master/docker/qa-service/docker-compose.yaml
 docker volume create qamg
 docker volume create qacode
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
 docker-compose up -d
