5670ba7b9c65e482e6dacae52a1212dbe510cf88
yutiansut <yutiansut@qq.com>
2019-08-18 15:46:15 +0800

    #update for saving stock_block

--- QUANTAXIS/QAFetch/QAQAWEB.py
+++ QUANTAXIS/QAFetch/QAQAWEB.py
@@ -27,11 +27,30 @@
 
 233333 结果变成自己访问自己了
 """
-import requests
-import tornado
+import  pandas as pd
+from QUANTAXIS.QAUtil.QACode import QA_util_code_tostr
 
 
-
-def QA_fetch_get_stock_day(code,start,end,ip='192.168.0.1',port='8010'):
+def QA_fetch_get_stock_day(code, start, end, ip='192.168.0.1', port='8010'):
     pass
-    #requests.get(
+    # requests.get(
+
+
+def QA_fetch_get_stock_block():
+    """ths的版块数据
+
+    Returns:
+        [type] -- [description]
+    """
+
+    url = 'https://gitee.com/yutiansut/QADATA/raw/master/self_block.csv'
+    try:
+        bl = pd.read_csv(url)
+        return bl.assign(code=bl['证券代码'].apply(QA_util_code_tostr), blockname=bl['行业'], name=bl['证券名称'], source='outside', type='outside').set_index('code', drop=False)
+    except Exception as e:
+        print(e)
+        return None
+
+
+if __name__ == "__main__":
+    print(QA_fetch_get_stock_block())
