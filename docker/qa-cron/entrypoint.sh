92919728093d11424f6d02ce3e0cfe91ef7d2828
yutiansut <yutiansut@qq.com>
2019-08-27 15:15:20 +0800

    #update financialmeans

--- docker/qa-cron/entrypoint.sh
+++ docker/qa-cron/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-sed -i "s|localhost|$MONGODB|" /QUANTAXIS/QUANTAXIS/QAUtil/QASetting.py
+sed -i "s|localhost|$MONGODB|" /usr/local/lib/python3.6/site-packages/QUANTAXIS/QAUtil/QASetting.py
 exec "$@"
