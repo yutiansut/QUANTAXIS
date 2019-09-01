ffb90422e274d5a6a8c4bde72c7174947e436913
hiteck88 <njumqi@hotmail.com>
2019-09-01 01:13:04 -0400

    添加三个命令可以单独保存daily data for stock,index,etf, e.g. save single_stock_day 600066, save single_index_day 000300, save single_etf_day 510050

--- QUANTAXIS/QASU/main.py
+++ QUANTAXIS/QASU/main.py
@@ -195,6 +195,27 @@ def QA_SU_save_stock_day(engine, client=DATABASE, paralleled=False):
     engine.QA_SU_save_stock_day(client=client)
 
 
+def QA_SU_save_single_stock_day(code, engine, client=DATABASE, paralleled=False):
+    """save stock_day
+
+    Arguments:
+        code: stock code
+        engine {[type]} -- [description]
+
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+
+    :param paralleled: 是否并行处理(default: {True})
+    """
+
+    engine = select_save_engine(engine, paralleled=paralleled)
+    engine.QA_SU_save_single_stock_day(code=code, client=client)
+
+
+
+
+
+
 def QA_SU_save_option_contract_list(engine, client=DATABASE):
     '''
 
@@ -300,6 +321,24 @@ def QA_SU_save_index_day(engine, client=DATABASE, paralleled=False):
     engine = select_save_engine(engine, paralleled=paralleled)
     engine.QA_SU_save_index_day(client=client)
 
+def QA_SU_save_single_index_day(code, engine, client=DATABASE, paralleled=False):
+    """save index_day
+
+    Arguments:
+        code: index code
+        engine {[type]} -- [description]
+
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+
+    :param paralleled: 是否并行处理(default: {True})
+    """
+
+    engine = select_save_engine(engine, paralleled=paralleled)
+    engine.QA_SU_save_single_index_day(code=code, client=client)
+
+
+
 
 def QA_SU_save_index_min(engine, client=DATABASE):
     """save index_min
@@ -328,6 +367,21 @@ def QA_SU_save_etf_day(engine, client=DATABASE, paralleled=False):
 
     engine = select_save_engine(engine, paralleled=paralleled)
     engine.QA_SU_save_etf_day(client=client)
+
+def QA_SU_save_single_etf_day(code, engine, client=DATABASE, paralleled=False):
+    """save etf_day
+
+    Arguments:
+        code: etf code
+        engine {[type]} -- [description]
+
+    Keyword Arguments:
+        client {[type]} -- [description] (default: {DATABASE})
+    """
+
+    engine = select_save_engine(engine, paralleled=paralleled)
+    engine.QA_SU_save_single_etf_day(code = code, client=client)
+
 
 
 def QA_SU_save_etf_min(engine, client=DATABASE):
