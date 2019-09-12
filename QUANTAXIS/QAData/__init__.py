3969980e68644679220467d00a8c577411a5a60b
nehcuh <huchen@szu.edu.cn>
2019-09-12 09:32:45 +0800

    improve stock transaction method and add index transaction dealing method

--- QUANTAXIS/QAData/__init__.py
+++ QUANTAXIS/QAData/__init__.py
@@ -43,11 +43,13 @@ from QUANTAXIS.QAData.QADataStruct import (QA_DataStruct_Day,
                                            QA_DataStruct_Future_min,
                                            QA_DataStruct_Index_day,
                                            QA_DataStruct_Index_min,
+                                           QA_DataStruct_Index_transaction,
                                            QA_DataStruct_Min,
                                            QA_DataStruct_Stock_day,
                                            QA_DataStruct_Stock_min,
                                            QA_DataStruct_Stock_realtime,
-                                           QA_DataStruct_Stock_transaction)
+                                           QA_DataStruct_Stock_transaction
+                                           )
 from QUANTAXIS.QAData.QAFinancialStruct import QA_DataStruct_Financial
 from QUANTAXIS.QAData.QAIndicatorStruct import QA_DataStruct_Indicators
 from QUANTAXIS.QAData.QASeriesStruct import QA_DataStruct_Series
