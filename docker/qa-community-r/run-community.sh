bd700bca3a303326df42920ac0fe08d0f511f22a
yutiansut <Yutiansut940809>
2020-12-22 04:24:31 +0800

    #update for qadocker

--- docker/qa-community-r/run-community.sh
+++ docker/qa-community-r/run-community.sh
@@ -2,4 +2,4 @@
 
 sed -i "s|localhost|$MONGODB|"  /opt/conda/lib/python3.7/site-packages/QUANTAXIS/QAUtil/QASetting.py
 jupyter lab --allow-root --config /root/.jupyter/jupyter_notebook_config.py &
-cd /root/QADESK_BASIC && python -m http.server 80
+cd /root/qamazing_community && python -m http.server 80
