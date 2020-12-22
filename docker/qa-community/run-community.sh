f4235c6d2edb6978460a7f6382d22e939ffa0fc7
yutiansut <Yutiansut940809>
2020-12-22 10:44:11 +0800

    #update for docker

--- docker/qa-community/run-community.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "s|localhost|$MONGODB|"  /opt/conda/lib/python3.8/site-packages/QUANTAXIS/QAUtil/QASetting.py
-jupyter lab --allow-root  --notebook-dir=~ &
-cd ~/qamazing_community && python -m http.server 80
--- docker/qa-cron/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-sed -i "s|localhost|$MONGODB|" /opt/conda/lib/python3.8/site-packages/QUANTAXIS/QAUtil/QASetting.py
-exec "$@"
