5ddd72ab55529ed85c8d4d566dd31b67d719d711
yutiansut <yutiansut@qq.com>
2021-02-18 03:09:26 +0800

    #update change default dir to /root

--- docker/qa-community-rust/run-community.sh
+++ docker/qa-community-rust/run-community.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sed -i "s|localhost|$MONGODB|"  /opt/conda/lib/python3.8/site-packages/QUANTAXIS/QAUtil/QASetting.py
-jupyter lab --allow-root  --notebook-dir=~ --config /root/.jupyter/jupyter_notebook_config.py &
+
+jupyter lab --allow-root  --notebook-dir=/root --config /root/.jupyter/jupyter_notebook_config.py &
 exec "$@"
 
 cd /root/qamazing_community && python -m http.server 80
