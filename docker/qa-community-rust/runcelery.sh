ca574f061b19b652f2e53a1d117bfbe2784aa5c9
yutiansut <yutiansut@qq.com>
2021-02-18 02:33:54 +0800

    #update for qacommunity

--- docker/qa-community-rust/runcelery.sh
+++ docker/qa-community-rust/runcelery.sh
@@ -7,6 +7,7 @@ echo $QARUN_AMQP
 quantaxis_webserver &
 qifi_manager &
 qavifiserver &
+/home/portfoliohandler &
 
 echo 'start quantaxis_run job worker'
 celery -A quantaxis_run worker --loglevel=info
