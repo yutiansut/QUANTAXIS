20062f7c9f4362e4a186a3356d39ce893167f333
wxj0916 <wxj0916@163.com>
2021-03-01 22:31:49 +0800

    Delete docker/kite/qa-community-rust directory

--- docker/kite/qa-community-rust/run-community.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i "s|localhost|$MONGODB|"  /opt/conda/lib/python3.8/site-packages/QUANTAXIS/QAUtil/QASetting.py
-
-jupyter lab --allow-root  --notebook-dir=/root --config /root/.jupyter/jupyter_notebook_config.py &
-exec "$@"
-
-cd /root/qamazing_community && python -m http.server 80
