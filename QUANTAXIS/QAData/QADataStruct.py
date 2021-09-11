eb306a1120819b32be8b8c2363a08dce5927379c
1106628276 <1106628276@qq.com>
2021-09-12 01:18:36 +0800

    fix: 复权因子只更新到昨天，当天的adj没有值，使用ffill填充

--- QUANTAXIS/QAData/QADataStruct.py
+++ QUANTAXIS/QAData/QADataStruct.py
@@ -160,6 +160,7 @@ class QA_DataStruct_Stock_day(_quotation_base):
                     ).set_index(['date',
                                  'code'])
                     data = self.data.join(adj)
+                    data['adj'].fillna(method='ffill', inplace=True)
                     for col in ['open', 'high', 'low', 'close']:
                         data[col] = data[col] * data['adj']
                     # data['volume'] = data['volume'] / \
@@ -365,7 +366,7 @@ class QA_DataStruct_Stock_min(_quotation_base):
                     u = u.set_index(['date', 'code'], drop=False)
 
                     data = u.join(adj).set_index(['datetime', 'code'])
-
+                    data['adj'].fillna(method='ffill', inplace=True)
                     for col in ['open', 'high', 'low', 'close']:
                         data[col] = data[col] * data['adj']
                     # data['volume'] = data['volume'] / \
