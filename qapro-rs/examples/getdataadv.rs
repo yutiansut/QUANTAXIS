13028ffa8b4f030acb803e05d3e0afbe770e6280
yutiansut <yutiansut@qq.com>
2021-12-27 10:35:15 +0800

    #rust example

--- qapro-rs/examples/getdataadv.rs
+++ qapro-rs/examples/getdataadv.rs
@@ -1,14 +1,14 @@
 use actix_rt;
-use chrono::Local;
+
 use qapro_rs::qaconnector::clickhouse::ckclient;
 use qapro_rs::qaconnector::clickhouse::ckclient::DataConnector;
 use qapro_rs::qadatastruct::stockday::QADataStruct_StockDay;
 use qapro_rs::qaenv::localenv::CONFIG;
 use qapro_rs::qalog::log4::init_log4;
 use qapro_rs::qaprotocol::mifi::qafastkline::QAKlineBase;
-use qapro_rs::qautil::tradedate::get_n_day_before_date9;
-use serde_json::Value;
-use std::collections::HashMap;
+
+use polars::prelude::{ChunkCompare, RollingOptions};
+use polars::series::ops::NullBehavior;
 use std::fmt::format;
 
 #[actix_rt::main]
@@ -26,7 +26,41 @@ async fn main() {
 
     let cache_file = format!("{}stockday.parquet", &CONFIG.DataPath.cache);
 
-    let data = QADataStruct_StockDay::new_from_parquet(cache_file.as_str());
+    let mut data = QADataStruct_StockDay::new_from_parquet(cache_file.as_str());
 
     println!("load cache file {:#?}", data.data);
+
+    println!(
+        "groupby test {:#?}",
+        data.data.groupby("date").unwrap().select("close").mean()
+    );
+    println!(
+        "groupby test {:#?}",
+        data.data
+            .groupby("order_book_id")
+            .unwrap()
+            .select("close")
+            .mean()
+    );
+
+    println!(
+        "diff test {:#?}",
+        data.data["high"].diff(1, NullBehavior::Drop)
+    );
+
+    let selectdf = data.query_code("300002.XSHE");
+    println!("select df {:#?}", selectdf);
+    let close = &selectdf["close"];
+    let lastclose = close.diff(1, NullBehavior::Ignore);
+    println!("pct test {:#?}", close / &lastclose);
+
+    let ma20 = close
+        .rolling_mean(RollingOptions {
+            window_size: 5,
+            min_periods: 1,
+            weights: None,
+            center: false,
+        })
+        .unwrap();
+    println!("rolling mean test {:#?}", ma20);
 }
