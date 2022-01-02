45cf6bed8a3fc83df9b44b8ce7eaffe3cd77fc2d
yutiansut <yutiansut@qq.com>
2022-01-03 03:35:48 +0800

    #

--- qapro-rs/examples/factorplayground.rs
+++ qapro-rs/examples/factorplayground.rs
@@ -12,6 +12,7 @@ use qapro_rs::qaconnector::clickhouse::ckclient;
 use qapro_rs::qaconnector::clickhouse::ckclient::DataConnector;
 use qapro_rs::qadatastruct::stockday::QADataStruct_StockDay;
 use qapro_rs::qaenv::localenv::CONFIG;
+use qapro_rs::qaaccount::account::QA_Account;
 
 #[actix_rt::main]
 async fn main() {
@@ -133,11 +134,35 @@ async fn main() {
     let dates = rank4["date"].utf8().unwrap();
     sw.restart();
 
+    let mut acc = QA_Account::new("test2", "test", "test", 1000000000.0, false, "backtest");
+    let mut curdate = "";
     for (code, date, close) in izip!(codes, dates, closes) {
         let code2: &str = code.unwrap();
         let date2: &str = date.unwrap();
         let close2: f32 = close.unwrap();
+        if curdate != date2 {
+            acc.settle();
+            curdate = date2;
+        } else {
+            let posx = acc.get_position(code2);
+            match posx {
+                Some(pos) => {
+                    if pos.volume_long_his > 0.0 {
+                        acc.sell(code2, 100.0, date2, close2 as f64);
+                    } else {
+                        if rand::random() {
+                            acc.buy(code2, 100.0, date2, close2 as f64);
+                        }
+                    }
+                }
+                _ => {
+                    acc.init_h(code2);
+                }
+            }
+        }
     }
 
+
     println!("calc get row time {:#?}", sw.elapsed());
+    acc.to_csv("".to_string());
 }
